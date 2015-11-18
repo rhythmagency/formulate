44de8b0845b81da77556220632585696d862fcd0
nicholas_westby <nicholas.westby@rhythmagency.com>
2015-11-17 16:14:03 -0800

    Created section with icon (FORM-2).

---
+++ formulate.app/Handlers/ApplicationStartedHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace formulate.app.Handlers
+{
+
+    // Namespaces.
+    using Umbraco.Core;
+
+
+    /// <summary>
+    /// Handles the application started event.
+    /// </summary>
+    internal class ApplicationStartedHandler : ApplicationEventHandler
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Application started.
+        /// </summary>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            var service = applicationContext.Services.SectionService;
+            var existingSection = service.GetByAlias("formulate");
+            if (existingSection == null)
+            {
+                service.MakeNew("Formulate", "formulate", "icon-formulate-clipboard", 6);
+            }
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ formulate.app/Install/Installer.cs
@@ -0,0 +1,8 @@
+﻿//TODO: This is a placeholder class for now.
+namespace formulate.app.Install
+{
+    internal class Installer
+    {
+        //TODO: For the user performing the install, give them permission to the new section.
+    }
+}
