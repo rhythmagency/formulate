9e050c6c1fbfbac7eb508a60b721a72ee7298372
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-11-28 00:14:34 -0800

    Added note for later (idea regarding package actions).

--- src/formulate.app/Install/Installer.cs
+++ src/formulate.app/Install/Installer.cs
@@ -4,5 +4,6 @@ namespace formulate.app.Install
     internal class Installer
     {
         //TODO: For the user performing the install, give them permission to the new section.
+        //TODO: This looks useful (xdt package action): http://www.nibble.be/?p=461
     }
 }
