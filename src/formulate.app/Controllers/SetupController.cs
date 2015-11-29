aaa6980399810cac916e80b56e33f76c385140d4
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-11-29 00:29:18 -0800

    Implemented functionality behind "Permit" button.

---
+++ src/formulate.app/Controllers/SetupController.cs
@@ -0,0 +1,99 @@
+﻿namespace formulate.app.Controllers
+{
+
+    // Namespaces.
+    using System;
+    using System.Web.Http;
+    using Umbraco.Core;
+    using Umbraco.Core.Logging;
+    using Umbraco.Web;
+    using Umbraco.Web.Editors;
+    using Umbraco.Web.Mvc;
+    using Umbraco.Web.WebApi.Filters;
+    using Constants = Umbraco.Core.Constants;
+
+
+    /// <summary>
+    /// Controller that handles operations related to setup of
+    /// Forulate.
+    /// </summary>
+    [PluginController("Formulate")]
+    [UmbracoApplicationAuthorize(Constants.Applications.Developer)]
+    public class SetupController : UmbracoAuthorizedJsonController
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SetupController()
+            : this(UmbracoContext.Current)
+        {
+        }
+
+
+        /// <summary>
+        /// Primary constructor.
+        /// </summary>
+        public SetupController(
+            UmbracoContext umbracoContext)
+            : base(umbracoContext)
+        {
+        }
+
+        #endregion
+
+
+        #region Web Methods
+
+        /// <summary>
+        /// Gives the current user permission to access the
+        /// "Formulate" section in Umbraco.
+        /// </summary>
+        /// <returns>
+        /// An object indicating whether or not the operation
+        /// was sucessful.
+        /// </returns>
+        [HttpPost]
+        public object PermitAccessToFormulate()
+        {
+            try
+            {
+
+                // Variables.
+                var services = ApplicationContext.Current.Services;
+                var service = services.UserService;
+                var security = UmbracoContext.Current.Security;
+                var currentUser = security.CurrentUser;
+                currentUser.AddAllowedSection("formulate");
+                service.Save(currentUser, true);
+
+
+                // Indicate success.
+                return new
+                {
+                    Success = true
+                };
+
+            }
+            catch (Exception ex)
+            {
+
+                // Error (log and indicate failure).
+                var message = "An error occurred while attempting to set permissions.";
+                LogHelper.Error<SetupController>(message, ex);
+                return new
+                {
+                    Success = false,
+                    Reason = message
+                };
+
+            }
+        }
+
+        #endregion
+
+    }
+
+}
--- src/formulate.app/JavaScript/Services/formulatePermissions.js
+++ src/formulate.app/JavaScript/Services/formulatePermissions.js
@@ -1,10 +1,79 @@
 // Service.
 var app = angular.module("umbraco");
-app.factory("formulatePermissions", function() {
+app.factory("formulatePermissions", function ($http, notificationsService) {
     return {
-        permitFormulateAccess: function(directive) {
-            //TODO: ...
-            alert("Permitting...");
-        }
+        permitFormulateAccess:
+            getPermitFormulateAccess($http, notificationsService)
     };
 });
+
+// Returns a function that will request that the current
+// user be given access to the "Formulate" section in Umbraco.
+function getPermitFormulateAccess($http, notificationsService) {
+    return function (directive) {
+
+        // Variables.
+        var url = "/umbraco/backoffice/Formulate/Setup/PermitAccessToFormulate";
+        var options = getReqestOptions();
+
+        // Send request to set permissions.
+        $http.post(url, null, options).success(function (data) {
+            if (data.Success) {
+
+                // Success notification.
+                showSuccess(notificationsService);
+
+                // Reload page.
+                reloadPage();
+
+            } else {
+
+                // Application error.
+                handleApplicationError(notificationsService, data.Reason);
+
+            }
+        }).error(function () {
+
+            // Server error.
+            handleServerError(notificationsService);
+
+        });
+
+    };
+}
+
+// Reloads the page after 3 seconds.
+function reloadPage() {
+    setTimeout(function () {
+        window.location.reload(true);
+    }, 3000);
+}
+
+// Handles an application error with a notification message.
+function handleApplicationError(notificationsService, message) {
+    var title = "Unexpected Error";
+    notificationsService.error(title, message);
+}
+
+// Handles a server error with a notification message.
+function handleServerError(notificationsService) {
+    var title = "Server Error";
+    var message = "There was an issue communicating with the server.";
+    notificationsService.error(title, message);
+}
+
+// Shows a success notification.
+function showSuccess(notificationsService) {
+    var title = "Success";
+    var message = "You have been granted access to the \"Formulate\" section. The page will automatically refresh in a few seconds.";
+    notificationsService.success(title, message);
+}
+
+// Gets the options variable for the HTTP request.
+function getReqestOptions() {
+    return {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+}
