fca1a060188e3c1f9bea659ade5a0e9552beba65
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-02-10 22:11:04 -0800

    Began refactoring and commenting layout directive.

--- src/formulate.app/Directives/layoutKinds/layoutBasic/layoutBasic.js
+++ src/formulate.app/Directives/layoutKinds/layoutBasic/layoutBasic.js
@@ -1,4 +1,8 @@
-﻿//TODO: Need to refactor and comment this file.
+﻿/**
+ * The "formulateLayoutBasic" directive allows the user to create
+ * a form layout that uses multiple rows and multiple columns. It
+ * only supports a few row configurations (i.e., 1-4 columns).
+ */
 
 // Variables.
 var app = angular.module("umbraco");
@@ -21,46 +25,92 @@ function directive(formulateDirectives) {
     };
 }
 
+// Controller.
 function controller($scope, formulateForms, dialogService) {
 
+    // Variables.
     var services = {
         $scope: $scope,
         formulateForms: formulateForms,
         dialogService: dialogService
     };
+
+    // Scope variables.
     $scope.editRows = false;
     $scope.allFields = [];
     $scope.rows = angular.copy($scope.data.rows || []);
-    $scope.getCellClass = function (row) {
+    $scope.fieldSortableOptions = getFieldSortableOptions();
+    $scope.rowsSortableOptions = getRowSortableOptions();
+    $scope.getCellClass = getGetCellClass();
+    $scope.deleteRow = getDeleteRow(services);
+    $scope.addRow = getAddRow(services);
+    $scope.pickForm = getPickForm(services);
+
+    // Initialize watchers.
+    watchEditRowsSetting(services);
+    watchRowChanges(services);
+
+    // Initialize.
+    if ($scope.data.formId) {
+        refreshFields($scope.data.formId, services);
+    } else {
+        checkFields($scope);
+    }
+
+}
+
+// Returns the function that returns a class for a cell in the
+// specified row.
+function getGetCellClass() {
+    return function (row) {
         return "span" + (12 / row.cells.length).toString();
     };
+}
 
-    $scope.sortableOptions = {
+// Returns the options to use when sorting fields.
+function getFieldSortableOptions() {
+    return {
         cursor: "move",
         connectWith: ".formulate-cell",
         tolerance: "pointer",
         items: ".formulate-cell-field",
         opacity: 0.5
     };
+}
 
-    $scope.rowsSortableOptions = {
+// Returns the options to use when sorting rows.
+function getRowSortableOptions() {
+    return {
         cursor: "move",
         tolerance: "pointer",
         axis: "y",
         opacity: 0.5,
         disabled: true
     };
+}
 
-    $scope.$watch("editRows", function (newValue, oldValue) {
+// Watches the "Edit Rows" setting to enable/disable sorting.
+function watchEditRowsSetting(services) {
+    var $scope = services.$scope;
+    $scope.$watch("editRows", function (newValue) {
         $scope.rowsSortableOptions.disabled = !newValue;
     });
+}
 
-    $scope.deleteRow = function (index) {
+// Returns a function that deletes the row at the specified index.
+function getDeleteRow(services) {
+    var $scope = services.$scope;
+    return function (index) {
         $scope.rows.splice(index, 1);
         checkFields($scope);
     };
+}
 
-    $scope.addRow = function(columnCount) {
+// Returns a function that adds a row with the specified number
+// of columns.
+function getAddRow(services) {
+    var $scope = services.$scope;
+    return function(columnCount) {
         var columns = [];
         for (var i = 0; i < columnCount; i++) {
             columns.push({
@@ -71,19 +121,15 @@ function controller($scope, formulateForms, dialogService) {
             cells: columns
         });
     };
+}
 
-    $scope.pickForm = getPickForm(services);
-
-    $scope.$watch("rows", function (newValue, oldValue) {
+// Watches for changes to the rows and updates the saved
+// data correspondingly.
+function watchRowChanges(services) {
+    var $scope = services.$scope;
+    $scope.$watch("rows", function (newValue) {
         updateDataRows(newValue, services);
     }, true);
-
-    if ($scope.data.formId) {
-        refreshFields($scope.data.formId, services);
-    } else {
-        checkFields($scope);
-    }
-
 }
 
 function updateDataRows(rows, services) {
@@ -157,7 +203,6 @@ function checkFields($scope) {
 
 function getPickForm(services) {
     var dialogService = services.dialogService;
-    var formulateForms = services.formulateForms;
     var $scope = services.$scope;
     return function() {
         dialogService.open({
