5fafcb1361df25c31f82b1e587d2b8b2bc5d1e52
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-03-29 09:55:14 -0700

    Fixed duplicate bug (was due to typo).

--- src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/rows.js
+++ src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/rows.js
@@ -19,7 +19,7 @@ function formulateRows() {
         template: '<div>' +
                 '<div class="row ib top" ng-repeat="row in ctrl.rows">' +
                     '<div class="col-xs-12" ng-class="ctrl.getColClass(cell)" ng-repeat="cell in row.cells">' +
-                        '<div ng-repeat="field in cell.fields track by filed.id">' +
+                        '<div ng-repeat="field in cell.fields track by field.id">' +
                             '<formulate-field class="formulate__field" field-id="{{field.id}}"></formulate-field>' +
                         '</div>' +
                     '</div>' +
