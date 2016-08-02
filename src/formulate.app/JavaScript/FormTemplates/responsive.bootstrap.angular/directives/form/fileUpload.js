943b6706475fb88a353efa0c26bcb815d9c327cf
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-08-02 15:36:14 -0700

    Made file upload classes Formulate-specific.

--- src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
+++ src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
@@ -17,14 +17,14 @@ function directive() {
             buttonLabel: "="
         },
         template:
-            '<div class="file-upload">' +
+            '<div class="formulate__file-upload">' +
                 '<!-- Can be styled with this approach: https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3 -->' +
                 '<!-- TLDR: Hide the file input, and make the label look like a button. -->' +
-                '<label class="file-upload__button">' +
-                    '<span class="file-upload__button-text">{{buttonLabel}}</span>'+
-                    '<input class="file-upload__input" type="file" formulate-file-change ng-model="fieldModel" />' +
+                '<label class="formulate__file-upload-button">' +
+                    '<span class="formulate__file-upload-button-text">{{buttonLabel}}</span>'+
+                    '<input class="formulate__file-upload-input" type="file" formulate-file-change ng-model="fieldModel" />' +
                 '</label>' +
-                '<span class="file-upload__filename">' +
+                '<span class="formulate__file-upload-filename">' +
                     '{{fieldModel.name}}' +
                 '</span>'+
             '</div>'
