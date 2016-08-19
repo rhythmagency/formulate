d113c738dd420371ac0431ba50e753ea77d52ea1
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-08-19 15:30:16 -0700

    Add ability to clear selected file upload: https://github.com/rhythmagency/formulate/issues/55

--- src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
+++ src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
@@ -6,11 +6,32 @@ var angular = require('angular');
 // App.
 var app = angular.module('formulate');
 
+// Controller.
+function Controller($scope) {
+    this.injected = {
+        $scope: $scope
+    };
+    $scope.uploadList = [{}];
+}
+
+// Clears the upload selection.
+Controller.prototype.clearSelection = function () {
+
+    // This is a trick to force Angular to render the file input again (thereby clearing the selection).
+    this.injected.$scope.uploadList = [{}];
+
+    // Clear the model.
+    this.injected.$scope.fieldModel = undefined;
+
+};
+
 // Directive.
 function directive() {
     return {
         restrict: "E",
         replace: true,
+        controller: Controller,
+        controllerAs: "ctrl",
         scope: {
             fieldId: "=",
             fieldModel: "=",
@@ -20,13 +41,18 @@ function directive() {
             '<div class="formulate__file-upload">' +
                 '<!-- Can be styled with this approach: https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3 -->' +
                 '<!-- TLDR: Hide the file input, and make the label look like a button. -->' +
-                '<label class="formulate__file-upload-button">' +
-                    '<span class="formulate__file-upload-button-text">{{buttonLabel}}</span>'+
-                    '<input class="formulate__file-upload-input" type="file" formulate-file-change ng-model="fieldModel" />' +
+                '<label class="formulate__file-upload-button" ng-repeat="uploadItem in uploadList">' +
+                    '<span class="formulate__file-upload-button-text">{{$parent.buttonLabel}}</span>'+
+                    '<input class="formulate__file-upload-input" type="file" formulate-file-change ng-model="$parent.fieldModel" />' +
                 '</label>' +
                 '<span class="formulate__file-upload-filename">' +
                     '{{fieldModel.name}}' +
                 '</span>'+
+                '<a class="formulate__file-upload-clear" ng-if="fieldModel.name" ng-click="ctrl.clearSelection()">' +
+                    '<span class="formulate__file-upload-clear--inner">' +
+                        'Clear Selection' +
+                    '</span>' +
+                '</a>'+
             '</div>'
     };
 }
