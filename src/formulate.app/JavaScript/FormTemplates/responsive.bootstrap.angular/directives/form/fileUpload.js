5937f2d492c071d5c7724c3bff76480baab8f6ff
josef <josef.kohout@rhythmagency.com>
2016-09-14 13:59:09 -0700

    Fixing fileUpload directive

--- src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
+++ src/formulate.app/JavaScript/FormTemplates/responsive.bootstrap.angular/directives/form/fileUpload.js
@@ -7,58 +7,53 @@ var angular = require('angular');
 var app = angular.module('formulate');
 
 // Controller.
-function Controller($scope) {
-    this.injected = {
-        $scope: $scope
-    };
-    $scope.uploadList = [{}];
+function FormulateFileUploadController() {
+    // This is a trick to force Angular to render the file input again (thereby clearing the selection).
+    this.uploadResetList = [{}];
 }
 
 // Clears the upload selection.
-Controller.prototype.clearSelection = function () {
-
-    // Variables.
-    var $scope = this.injected.$scope;
-
+FormulateFileUploadController.prototype.clearSelection = function () {
     // This is a trick to force Angular to render the file input again (thereby clearing the selection).
-    $scope.uploadList = [{}];
+    this.uploadResetList = [{}];
 
     // Clear the model.
-    $scope.fieldModel = undefined;
-
+    this.fieldModel = undefined;
 };
 
 // Directive.
-function directive() {
+function formulateFileUpload() {
     return {
         restrict: "E",
         replace: true,
-        controller: Controller,
+
+        controller: FormulateFileUploadController,
         controllerAs: "ctrl",
+        bindToController: true,
+
         scope: {
             fieldId: "=",
             fieldModel: "=",
             buttonLabel: "="
         },
+
         template:
             '<div class="formulate__file-upload">' +
-                '<div ng-repeat="uploadItem in uploadList">' +
+                '<div ng-repeat="uploadItem in ctrl.uploadResetList">' +
                     '<!-- Can be styled with this approach: https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3 -->' +
                     '<!-- TLDR: Hide the file input, and make the label look like a button. -->' +
                     '<label class="formulate__file-upload-button">' +
-                        '<span class="formulate__file-upload-button-text">{{$parent.buttonLabel}}</span>' +
-                        '<input class="formulate__file-upload-input" type="file" formulate-file-change ng-model="$parent.fieldModel" />' +
+                        '<span class="formulate__file-upload-button-text" ng-bind="ctrl.buttonLabel"></span>' +
+                        '<input class="formulate__file-upload-input" type="file" formulate-file-change ng-model="ctrl.fieldModel" />' +
                     '</label>' +
-                    '<span class="formulate__file-upload-filename">' +
-                        '{{$parent.fieldModel.name}}' +
-                    '</span>' +
-                    '<a href class="formulate__file-upload-clear" ng-if="$parent.fieldModel.name" ng-click="ctrl.clearSelection()">' +
-                        '<span class="formulate__file-upload-clear--inner">' +
-                            'Clear Selection' +
-                        '</span>' +
+
+                    '<span class="formulate__file-upload-filename" ng-bind="ctrl.fieldModel.name"></span>' +
+
+                    '<a href class="formulate__file-upload-clear" ng-if="ctrl.fieldModel.name" ng-click="ctrl.clearSelection()">' +
+                        '<span class="formulate__file-upload-clear--inner">Clear Selection</span>' +
                     '</a>' +
                 '</div>' +
             '</div>'
     };
 }
-app.directive("formulateFileUpload", directive);
+app.directive("formulateFileUpload", formulateFileUpload);
