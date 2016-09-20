bf28ad8139ca4923eae946724898f565e3fab696
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-09-20 11:14:56 -0700

    Implemented the backend for a date field.

---
+++ src/formulate.app/Directives/fields/dateField/dateField.js
@@ -0,0 +1,14 @@
+﻿// Variables.
+var app = angular.module("umbraco");
+
+// Associate directive.
+app.directive("formulateDateField", directive);
+
+// Directive.
+function directive(formulateDirectives) {
+    return {
+        restrict: "E",
+        replace: true,
+        template: formulateDirectives.get("fields/dateField/dateField.html")
+    };
+}
