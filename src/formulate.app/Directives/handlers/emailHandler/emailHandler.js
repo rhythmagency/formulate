3ad6ab4327f72f1268b3458145f51ef8c09ea920
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-01-06 11:06:50 -0800

    Finished implementing the back office portion for the email by field functionality.

--- src/formulate.app/Directives/handlers/emailHandler/emailHandler.js
+++ src/formulate.app/Directives/handlers/emailHandler/emailHandler.js
@@ -3,7 +3,6 @@ var app = angular.module("umbraco");
 
 // Associate directive/controller.
 app.directive("formulateEmailHandler", directive);
-app.controller("formulate.emailHandler", controller);
 
 // Directive.
 function directive(formulateDirectives) {
@@ -16,48 +15,167 @@ function directive(formulateDirectives) {
             configuration: "=",
             fields: "="
         },
-        controller: "formulate.emailHandler"
+        controller: Controller
     };
 }
 
 // Controller.
-function controller($scope) {
+function Controller($scope, notificationsService) {
+
+    // Variables.
+    this.injected = {
+        $scope: $scope,
+        notificationsService: notificationsService
+    };
+
+    // Initialize scope variables.
     $scope.tempData = {
-        recipientEmailField: null
+        recipientEmailField: null,
+        recipientFields: []
     };
     if (!$scope.configuration.recipients) {
         $scope.configuration.recipients = [];
     }
+    this.initializeRecipientFields();
+
+    // Add scope functions.
+    $scope.addRecipient = this.addRecipient.bind(this);
+    $scope.deleteRecipient = this.deleteRecipient.bind(this);
+    $scope.addRecipientField = this.addRecipientField.bind(this);
+    $scope.deleteRecipientField = this.deleteRecipientField.bind(this);
+    $scope.deleteRecipientField = this.deleteRecipientField.bind(this);
+
+    // Start watching changes to the recipient email fields.
+    this.watchRecipientFieldChanges();
+
+}
+
+/**
+ * Adds a blank email address to the list of recipients of emails.
+ */
+Controller.prototype.addRecipient = function () {
+    var $scope = this.injected.$scope;
+    $scope.configuration.recipients.push({
+        email: ""
+    });
+};
+
+/**
+ * Deletes the email recipient at the specified index.
+ * @param index The index of the email recipient to delete.
+ */
+Controller.prototype.deleteRecipient = function (index) {
+    var $scope = this.injected.$scope;
+    $scope.configuration.recipients.splice(index, 1);
+};
+
+/**
+ * Adds the currently selected field to the list of email recipient fields.
+ */
+Controller.prototype.addRecipientField = function () {
+
+    // Variables.
+    var $scope = this.injected.$scope;
+    var notificationsService = this.injected.notificationsService;
+    var id = $scope.tempData.recipientEmailField;
+
+    // Ensure the field to add wasn't already added (don't want duplicates).
+    for (var i = 0; i < $scope.tempData.recipientFields.length; i++) {
+        var field = $scope.tempData.recipientFields[i];
+        if (field.id === id) {
+            var title = "Duplicate Field";
+            var message = "That field was already added. You cannot add the same field twice.";
+            notificationsService.error(title, message);
+            return;
+        }
+    }
+
+    // Get the field based on the ID selected in the field selection drop down.
+    var foundField = this.findFieldWithId(id);
+    if (!foundField) {
+        return;
+    }
+
+    // Add the field to the list of recipient fields.
+    $scope.tempData.recipientFields.push(foundField);
+
+};
+
+/**
+ * Deletes the email recipient field at the specified index.
+ * @param index The index of the email recipient field to delete.
+ */
+Controller.prototype.deleteRecipientField = function (index) {
+    var $scope = this.injected.$scope;
+    $scope.tempData.recipientFields.splice(index, 1);
+};
+
+/**
+ * Initializes the recipient fields based on the stored configuration of field ID's.
+ */
+Controller.prototype.initializeRecipientFields = function () {
+
+    // Variables.
+    var $scope = this.injected.$scope;
+    var fields = [];
+
+    // Ensure the configuration contains a recipient fields array.
     if (!$scope.configuration.recipientFields) {
         $scope.configuration.recipientFields = [];
     }
-    $scope.addRecipient = function () {
-        $scope.configuration.recipients.push({
-            email: ""
-        });
-    };
-    $scope.deleteRecipient = function (index) {
-        $scope.configuration.recipients.splice(index, 1);
-    };
-    $scope.addRecipientField = function () {
-        var id = $scope.tempData.recipientEmailField;
-        if (!id) {
-            return;
+
+    // Based on the stored recipient field ID's, create an array containing
+    // the full information about the fields with those ID's.
+    for (var i = 0; i < $scope.configuration.recipientFields.length; i++) {
+        var fieldId = $scope.configuration.recipientFields[i];
+        var field = this.findFieldWithId(fieldId);
+        if (field) {
+            fields.push(field);
+        } else {
+            fields.push({
+                id: fieldId,
+                name: "Unknown Field"
+            });
         }
-        var foundField = null;
-        for (var i = 0; i < $scope.fields.length; i++) {
-            var field = $scope.fields[i];
-            if (field.id === id) {
-                foundField = field;
-                break;
-            }
+    }
+
+    // Store the full field information to a temporary array used for display.
+    $scope.tempData.recipientFields = fields;
+
+};
+
+/**
+ * Attempts to get the field with the specified field ID.
+ * @param id The field ID.
+ * @returns {*} The field, if one matching the ID could be found; otherwise, null.
+ */
+Controller.prototype.findFieldWithId = function (id) {
+    var $scope = this.injected.$scope;
+    if (!id) {
+        return null;
+    }
+    var foundField = null;
+    for (var i = 0; i < $scope.fields.length; i++) {
+        var field = $scope.fields[i];
+        if (field.id === id) {
+            foundField = field;
+            break;
         }
-        if (!foundField) {
-            return;
+    }
+    return foundField;
+};
+
+/**
+ * Sets up a scope watcher to update the stored field ID's whenever the user changes the
+ * recipient email fields.
+ */
+Controller.prototype.watchRecipientFieldChanges = function () {
+    var $scope = this.injected.$scope;
+    $scope.$watchCollection("tempData.recipientFields", function (fields) {
+        var ids = [];
+        for (var i = 0; i < fields.length; i++) {
+            ids.push(fields[i].id);
         }
-        $scope.configuration.recipientFields.push(foundField);
-    };
-    $scope.deleteRecipientField = function (index) {
-        $scope.configuration.recipientFields.splice(index, 1);
-    };
-}
+        $scope.configuration.recipientFields = ids;
+    });
+};
