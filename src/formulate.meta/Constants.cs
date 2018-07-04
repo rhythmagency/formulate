303118e6f5a546a835eb979432bcf8460b2f4eb0
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2018-07-03 21:45:46 -0700

    Incremented version number to 1.5.0.0.

--- src/formulate.meta/Constants.cs
+++ src/formulate.meta/Constants.cs
@@ -17,7 +17,7 @@
         /// Do not reformat this code. A grunt task reads this
         /// version number with a regular expression.
         /// </remarks>
-        public const string Version = "1.4.1.0";
+        public const string Version = "1.5.0.0";
 
 
         /// <summary>
