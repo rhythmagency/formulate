dea5d179096b38026eda0c0ae20d46a98bba3dbd
jamiepollock <jamie.does.umbraco@gmail.com>
2019-06-03 18:08:10 -0500

    Added constant for ConfigFiles.

---
+++ src/formulate.app/Constants/Configuration/ConfigFiles.cs
@@ -0,0 +1,13 @@
+﻿namespace formulate.app.Constants.Configuration
+{
+    /// <summary>
+    /// Constants for config file paths.
+    /// </summary>
+    public static class ConfigFilePaths
+    {
+        /// <summary>
+        /// The formulate config path.
+        /// </summary>
+        public const string FormulateConfigPath = "~/App_Plugins/Formulate/FormulateConfiguration.json";
+    }
+}
