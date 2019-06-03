1a35728e05beea2a7ccb6023aaf6183cc19dd817
jamiepollock <jamie.does.umbraco@gmail.com>
2019-06-03 16:19:40 -0500

    Provided default values for PersistenceConfig properties

--- src/formulate.app/Configuration/PersistenceConfig.cs
+++ src/formulate.app/Configuration/PersistenceConfig.cs
@@ -1,5 +1,7 @@
 namespace formulate.app.Configuration
 {
+    using System.ComponentModel;
+
     /// <summary>
     /// The persistence config.
     /// </summary>
@@ -8,11 +10,13 @@ namespace formulate.app.Configuration
         /// <summary>
         /// Gets or sets the json base path.
         /// </summary>
+        [DefaultValue("~/App_Data/Formulate/Json/")]
         public string JsonBasePath { get; set; }
 
         /// <summary>
         /// Gets or sets the file storage base path.
         /// </summary>
+        [DefaultValue("~/App_Data/Formulate/FileStorage/Files/")]
         public string FileStorageBasePath { get; set; }
     }
 }
