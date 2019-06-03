c6eb698fe10783608e8d71ba28f53dc6e94dd4b8
jamiepollock <jamie.does.umbraco@gmail.com>
2019-06-03 16:05:57 -0500

    Added TemplateConfigItem

---
+++ src/formulate.app/Configuration/TemplateConfigItem.cs
@@ -0,0 +1,30 @@
+﻿namespace formulate.app.Configuration
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The template config item.
+    /// </summary>
+    public sealed class TemplateConfigItem
+    {
+        /// <summary>
+        /// Gets or sets the template name.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the template path.
+        /// </summary>
+        [Required]
+
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets or sets the template id.
+        /// </summary>
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
