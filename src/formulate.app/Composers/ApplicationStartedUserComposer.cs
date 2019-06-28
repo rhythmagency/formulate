90e98bc197658d4d32fbf13a01295f2f5b370d4b
jamiepollock <jamie.does.umbraco@gmail.com>
2019-06-28 16:28:55 -0500

    Tidy up of ApplicationStartedUserComposer.

--- src/formulate.app/Composers/ApplicationStartedUserComposer.cs
+++ src/formulate.app/Composers/ApplicationStartedUserComposer.cs
@@ -1,8 +1,6 @@
 ﻿namespace formulate.app.Composers
 {
     // Namespaces.
-    using System.Configuration;
-
     using formulate.app.Backoffice;
     using formulate.app.Backoffice.Dashboards;
     using formulate.app.Components;
@@ -14,23 +12,12 @@
     using Umbraco.Core.Composing;
     using Umbraco.Web;
 
-    using MetaConstants = meta.Constants;
-
     /// <summary>
     /// Handles the application started event.
     /// </summary>
     [RuntimeLevel(MinLevel = RuntimeLevel.Install)]
     public class ApplicationStartedUserComposer : IUserComposer
     {
-        #region Constants
-
-        private const string DeveloperSectionXPath = @"/dashBoard/section[@alias='StartupDeveloperDashboardSection']";
-        private const string MissingDeveloperSection = @"Unable to locate StartupDeveloperDashboardSection in the dashboard.config. The Formulate tab will not be added to the Developer section.";
-        private const string InstallActionsError = @"An unknown error occurred while attempting to asynchronously run the install actions for Formulate.";
-        private const string TableCreationError = @"An error occurred while attempting to create the FormulateSubmissions table.";
-
-        #endregion
-
         #region Methods
 
         /// <summary>
@@ -76,24 +63,6 @@
         private void InitializeServerVariables(Composition composition)
         {
             composition.Components().Append<ServerVariablesComponent>();
-        }
-
-        
-
-        /// <summary>
-        /// Indicates whether or not the application setting with the specified key has a non-empty
-        /// value in the web.config.
-        /// </summary>
-        /// <param name="key">
-        /// The application setting key.
-        /// </param>
-        /// <returns>
-        /// True, if the value in the web.config is non-empty; otherwise, false.
-        /// </returns>
-        private bool DoesAppSettingExist(string key)
-        {
-            var value = ConfigurationManager.AppSettings[key];
-            return !string.IsNullOrWhiteSpace(value);
         }
 
         /// <summary>
