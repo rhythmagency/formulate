37b921a392b1097e8f8d16a6fc2f2ddc47d0819c
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2020-05-30 13:17:21 -0700

    fix (Initial Values) preopopulate field values based on query string
    See: https://github.com/rhythmagency/formulate/issues/172

--- src/formulate.frontend/responsive.plain-javascript/steps/render-form.js
+++ src/formulate.frontend/responsive.plain-javascript/steps/render-form.js
@@ -59,6 +59,7 @@ function renderForm(formData, formElement, placeholderElement, fieldRenderers, f
                 });
                 renderedFields.push(renderedField);
                 fieldElement = renderedField.getElement();
+                prepopulateValue(fieldElement, field.initialValue)
                 cellElement.appendChild(fieldElement);
 
             }
@@ -69,6 +70,58 @@ function renderForm(formData, formElement, placeholderElement, fieldRenderers, f
 
     // Return the rendered fields.
     return renderedFields;
+
+}
+
+/**
+ * Sets the initial values of fields.
+ * @param element The field wrapper element.
+ * @param values The array of values.
+ */
+function prepopulateValue(element, values) {
+
+    // Exit early if the element or values are invalid.
+    if (!element || !values || !values.length) {
+        return;
+    }
+
+    // Variables.
+    let inputs = element.querySelectorAll('input, textarea, select'),
+        input, i;
+
+    // Exit early if there are no inputs.
+    if (!inputs.length) {
+        return;
+    }
+
+    // Is this a single input, or a list of inputs?
+    if (inputs.length === 1) {
+
+        // Is this a checkbox?
+        input = inputs[0];
+        if (input.type === 'checkbox') {
+            input.checked = true;
+        }
+        else if (values.length === 1) {
+
+            // Likely a text field.
+            input.value = values[0];
+
+        }
+
+    } else {
+
+        // Loop over list of inputs (checkboxes or radio buttons).
+        for (i = 0; i < inputs.length; i++) {
+            input = inputs[i];
+            if (['checkbox', 'radio'].indexOf(input.type) >= 0) {
+                if (values.indexOf(input.value) >= 0) {
+                    input.checked = true;
+                }
+            }
+        }
+
+    }
 
 }
 
