7c66d19e96ba842c630409fdcbf370cd13f76f88
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2020-09-19 12:30:23 -0700

    feat (Version) incremented to 3.3.5

--- src/formulate.meta/Constants.cs
+++ src/formulate.meta/Constants.cs
@@ -17,7 +17,7 @@
         /// Do not reformat this code. A grunt task reads this
         /// version number with a regular expression.
         /// </remarks>
-        public const string Version = "3.3.4";
+        public const string Version = "3.3.5";
 
 
         /// <summary>
