77ae855f72cbf135b278685c8963e60bc78dc2bf
jamiepollock <jamie.does.umbraco@gmail.com>
2020-10-13 15:40:36 -0500

    [Code review] Updated LayoutBasic constants class with xml-docs.

--- src/formulate.app/Constants/Layouts/LayoutBasic.cs
+++ src/formulate.app/Constants/Layouts/LayoutBasic.cs
@@ -1,9 +1,23 @@
 ﻿namespace formulate.app.Constants.Layouts
 {
-    public class LayoutBasic
+    /// <summary>
+    /// Constants related to <see cref="app.Layouts.Kinds.Basic.LayoutBasic"/>.
+    /// </summary>
+    internal static class LayoutBasic
     {
+        /// <summary>
+        /// The id.
+        /// </summary>
         public const string Id = "B03310E9320744DCBE96BE0CF4F26C59";
+
+        /// <summary>
+        /// The name.
+        /// </summary>
         public const string Name = "Basic Layout";
+
+        /// <summary>
+        /// The directive.
+        /// </summary>
         public const string Directive = "formulate-layout-basic";
     }
 }
