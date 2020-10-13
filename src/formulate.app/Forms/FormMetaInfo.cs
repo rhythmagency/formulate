0b02664d3d7f40d7d6761f2da9e0d8efe2e9d27a
jamiepollock <jamie.does.umbraco@gmail.com>
2020-10-13 13:21:25 -0500

    [Code review] Formatting for MetaInfo classes.

--- src/formulate.app/Forms/FormMetaInfo.cs
+++ src/formulate.app/Forms/FormMetaInfo.cs
@@ -2,7 +2,6 @@
 {
 
     // Namespaces.
-
     using System;
 
     using Newtonsoft.Json;
@@ -10,43 +9,32 @@
     /// <summary>
     /// Information attached to a form.
     /// </summary>
+    /// <typeparam name="T">The type of data used by this form meta information.</typeparam>
     public class FormMetaInfo<T> : IFormMetaInfo
     {
-
         #region Properties
 
         /// <summary>
-        /// The alias of this form meta information.
+        /// Gets or sets the alias of this form meta information.
         /// </summary>
         public string Alias { get; set; }
 
-
         /// <summary>
-        /// The name of this form meta information.
+        /// Gets or sets the name of this form meta information.
         /// </summary>
         public string Name { get; set; }
 
-
         /// <summary>
-        /// The data stored by this form meta information.
+        /// Gets or sets the data stored by this form meta information.
         /// </summary>
         public T Data { get; set; }
 
-
         /// <summary>
-        /// The type of data stored by this form meta information.
+        /// Gets or sets the type of data stored by this form meta information.
         /// </summary>
         [JsonIgnore]
-        public Type ValueType
-        {
-            get
-            {
-                return typeof(T);
-            }
-        }
+        public Type ValueType => typeof(T);
 
         #endregion
-
     }
-
 }
