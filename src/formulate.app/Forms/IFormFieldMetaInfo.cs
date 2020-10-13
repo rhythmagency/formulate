455cf6adf02eb53fce16be1f264781c144172583
jamiepollock <jamie.does.umbraco@gmail.com>
2020-10-13 13:52:38 -0500

    [Code review] IFormFieldMetaInfo formatting and minor tweak to IFormFieldType.

--- src/formulate.app/Forms/IFormFieldMetaInfo.cs
+++ src/formulate.app/Forms/IFormFieldMetaInfo.cs
@@ -4,15 +4,24 @@
     // Namespaces.
     using System;
 
-
     /// <summary>
     /// The interface for all form field meta information.
     /// </summary>
     public interface IFormFieldMetaInfo
     {
+        /// <summary>
+        /// Gets or sets the alias.
+        /// </summary>
         string Alias { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Gets the value type.
+        /// </summary>
         Type ValueType { get; }
     }
-
 }
