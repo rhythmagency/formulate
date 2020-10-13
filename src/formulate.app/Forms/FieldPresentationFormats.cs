7ec6c4ae6189e39e3c88e8d1f05395143889587d
jamiepollock <jamie.does.umbraco@gmail.com>
2020-10-13 13:06:14 -0500

    [Code review] Formatting FieldPresentationFormats.

--- src/formulate.app/Forms/FieldPresentationFormats.cs
+++ src/formulate.app/Forms/FieldPresentationFormats.cs
@@ -1,10 +1,28 @@
 ﻿namespace formulate.app.Forms
 {
+    /// <summary>
+    /// Values used to determine how the current form field should be formatted.
+    /// </summary>
     public enum FieldPresentationFormats
     {
+        /// <summary>
+        /// The default option.
+        /// </summary>
         Unspecified,
+
+        /// <summary>
+        /// Detemines this field should be formatted for email messages.
+        /// </summary>
         Email,
+
+        /// <summary>
+        /// Detemines this field should be formatted for storage (e.g. database or file).
+        /// </summary>
         Storage,
+
+        /// <summary>
+        /// Determines this field should be formatted for transmission.
+        /// </summary>
         Transmission
     }
 }
