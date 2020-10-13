c3a4052176d8d33aab01a63fae6ddeef241c417d
jamiepollock <jamie.does.umbraco@gmail.com>
2020-10-13 17:30:16 -0500

    [Code review] formatting for DataInterfaces.

--- src/formulate.app/DataValues/DataInterfaces/IGetStringCollection.cs
+++ src/formulate.app/DataValues/DataInterfaces/IGetStringCollection.cs
@@ -4,13 +4,16 @@
     // Namespaces.
     using System.Collections.Generic;
 
-
     /// <summary>
     /// Any data values implementing this interface can return a collection of strings.
     /// </summary>
     public interface IGetStringCollection
     {
+        /// <summary>
+        /// Attempts to get a collection of strings for the given raw data.
+        /// </summary>
+        /// <param name="rawData">The raw data.</param>
+        /// <returns>A collection of <see cref="string"/>s.</returns>
         IEnumerable<string> GetValues(string rawData);
     }
-
 }
--- src/formulate.app/DataValues/DataInterfaces/IGetValueAndLabelCollection.cs
+++ src/formulate.app/DataValues/DataInterfaces/IGetValueAndLabelCollection.cs
@@ -5,13 +5,20 @@
     using core.Types;
     using System.Collections.Generic;
 
-
     /// <summary>
     /// Any data values implementing this interface can return a collection of value and label items.
     /// </summary>
     public interface IGetValueAndLabelCollection
     {
+        /// <summary>
+        /// Attempts to get a collection of <see cref="ValueAndLabel"/> for the given raw data.
+        /// </summary>
+        /// <param name="rawData">
+        /// The raw data.
+        /// </param>
+        /// <returns>
+        /// A collection of <see cref="ValueAndLabel"/>.
+        /// </returns>
         IEnumerable<ValueAndLabel> GetValues(string rawData);
     }
-
 }
