d5f52adb5fba78f44bc0e4fad6a1fcfb4bfbe1d5
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-22 09:10:47 -0400

    [v4] Added Validation Type interface.

---
+++ src/Formulate.Core/Types/IValidationType.cs
@@ -0,0 +1,9 @@
+﻿namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// A contract for creating a validation type.
+    /// </summary>
+    public interface IValidationType : IFormulateType
+    {
+    }
+}
