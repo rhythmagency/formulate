9d1d170e5f5478f4a992a7f6caac49a8017d720b
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-21 21:22:54 -0400

    [v4] Added IFormulateType.

---
+++ src/Formulate.Core/Types/IFormulateType.cs
@@ -0,0 +1,15 @@
+﻿namespace Formulate.Core.Types
+{
+    using System;
+
+    /// <summary>
+    /// The underlying type for other types identified by a Type ID.
+    /// </summary>
+    public interface IFormulateType
+    {
+        /// <summary>
+        /// Gets the Type ID.
+        /// </summary>
+        Guid TypeId { get; }
+    }
+}
