13ee40171d520117b1f7a7421654d1de5b5350f9
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-21 21:37:36 -0400

    [v4] Added FormulateTypeCollectionExtensions.

---
+++ src/Formulate.Core/Types/FormulateTypeCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// Extension methods that augments collections of <see cref="IFormulateType"/>,
+    /// </summary>
+    public static class FormulateTypeCollectionExtensions
+    {
+        /// <summary>
+        /// Gets the first or default <see cref="IFormulateType"/> which matches the provided <see cref="Guid"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection.</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="id">The id.</param>
+        /// <returns>A <typeparamref name="T"/> or default.</returns>
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Guid id) where T : IFormulateType
+        {
+            return collection is null ? default : collection.FirstOrDefault(x => x.TypeId == id);
+        }
+    }
+}
