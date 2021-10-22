8e0e177fe86e732c8e82a34fdfaaae253f0f9b0c
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-21 22:04:26 -0400

    [v4] Added Umbraco Builder extensions for IFormulateType collection builders.

---
+++ src/Formulate.Core/Types/UmbracoBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// Extension methods that augments <see cref="IUmbracoBuilder"/> to add <see cref="IFormulateType"/> collection builders.
+    /// </summary>
+    public static class UmbracoBuilderExtensions
+    {
+        /// <summary>
+        /// Gets the builder collection for adding <see cref="IDataValuesType"/> implementations.
+        /// </summary>
+        /// <param name="builder">The Umbraco builder.</param>
+        /// <returns>A <see cref="DataValuesTypeCollectionBuilder"/>.</returns>
+        public static DataValuesTypeCollectionBuilder DataValuesTypes(this IUmbracoBuilder builder)
+        {
+            return builder.WithCollectionBuilder<DataValuesTypeCollectionBuilder>();
+        }
+
+        /// <summary>
+        /// Gets the builder collection for adding <see cref="IFormFieldType"/> implementations.
+        /// </summary>
+        /// <param name="builder">The Umbraco builder.</param>
+        /// <returns>A <see cref="FormFieldTypeCollectionBuilder"/>.</returns>
+        public static FormFieldTypeCollectionBuilder FormFieldTypes(this IUmbracoBuilder builder)
+        {
+            return builder.WithCollectionBuilder<FormFieldTypeCollectionBuilder>();
+        }
+
+        /// <summary>
+        /// Gets the builder collection for adding <see cref="IFormHandlerType"/> implementations.
+        /// </summary>
+        /// <param name="builder">The Umbraco builder.</param>
+        /// <returns>A <see cref="LayoutTypeCollectionBuilder"/>.</returns>
+        public static FormHandlerTypeCollectionBuilder FormHandlerTypes(this IUmbracoBuilder builder)
+        {
+            return builder.WithCollectionBuilder<FormHandlerTypeCollectionBuilder>();
+        }
+
+        /// <summary>
+        /// Gets the builder collection for adding <see cref="ILayoutType"/> implementations.
+        /// </summary>
+        /// <param name="builder">The Umbraco builder.</param>
+        /// <returns>A <see cref="LayoutTypeCollectionBuilder"/>.</returns>
+        public static LayoutTypeCollectionBuilder LayoutTypes(this IUmbracoBuilder builder)
+        {
+            return builder.WithCollectionBuilder<LayoutTypeCollectionBuilder>();
+        }
+    }
+}
