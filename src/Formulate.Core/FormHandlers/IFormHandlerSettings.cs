f37838ed81937a06f6e31787eb9b7a6be4b18d78
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-23 15:20:48 -0400

    [v4] Introduced IFormHandlerSettings. These the are settings we would expect from Formulate for creating a new instance of a Form Handler.

---
+++ src/Formulate.Core/FormHandlers/IFormHandlerSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Formulate.Core.Types;
+
+namespace Formulate.Core.FormHandlers
+{
+    /// <summary>
+    /// Settings for creating a form handler.
+    /// </summary>
+    public interface IFormHandlerSettings : IFormulateType
+    {
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        Guid Id { get; }
+        
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the alias.
+        /// </summary>
+        string Alias { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this is enabled.
+        /// </summary>
+        bool Enabled { get; }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        string Configuration { get; }
+    }
+}
