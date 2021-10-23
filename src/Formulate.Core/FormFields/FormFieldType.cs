326ee901e4d7acf3026ed410a0001a1070c6848c
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-23 17:59:53 -0400

    [v4] Expanded upon IFormFieldType's properties. Added abstract FormFieldType to provide helpful defaults to extended fields where possible.

---
+++ src/Formulate.Core/FormFields/FormFieldType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Formulate.Core.FormFields
+{
+    /// <summary>
+    /// An abstract class for creating a new <see cref="IFormFieldType"/>.
+    /// </summary>
+    /// <remarks>It is not necessary to implement from this class but it does provide some helpful defaults for certain optional fields.</remarks>
+    public abstract class FormFieldType : IFormFieldType
+    {
+        /// <inheritdoc />
+        public abstract Guid TypeId { get; }
+        
+        /// <inheritdoc />
+        public abstract string TypeLabel { get; }
+
+        /// <inheritdoc />
+        public abstract string Icon { get; }
+
+        /// <inheritdoc />
+        public abstract string Directive { get; }
+
+        /// <inheritdoc />
+        /// <remarks>Defaults to false.</remarks>
+        public virtual bool IsTransitory => false;
+
+        /// <inheritdoc />
+        /// <remarks>Defaults to false.</remarks>
+        public virtual bool IsServerSideOnly => false;
+
+        /// <inheritdoc />
+        /// <remarks>Defaults to false.</remarks>
+        public virtual bool IsHidden => false;
+
+        /// <inheritdoc />
+        /// <remarks>Defaults to true.</remarks>
+        public virtual bool IsStored => true;
+
+        /// <inheritdoc />
+        public abstract IFormField CreateField(IFormFieldSettings settings);
+    }
+}
--- src/Formulate.Core/FormFields/IFormFieldType.cs
+++ src/Formulate.Core/FormFields/IFormFieldType.cs
@@ -8,6 +8,46 @@ namespace Formulate.Core.FormFields
     /// </summary>
     public interface IFormFieldType : IFormulateType, IDiscoverable
     {
+        /// <summary>
+        /// Gets the type label.
+        /// </summary>
+        string TypeLabel { get; }
+
+        /// <summary>
+        /// Gets the icon.
+        /// </summary>
+        string Icon { get; }
+
+        /// <summary>
+        /// Gets the directive.
+        /// </summary>
+        string Directive { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field type is transitory.
+        /// </summary>
+        bool IsTransitory { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field type is server side only.
+        /// </summary>
+        bool IsServerSideOnly { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field type is hidden.
+        /// </summary>
+        bool IsHidden { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field type is stored.
+        /// </summary>
+        bool IsStored { get; }
+
+        /// <summary>
+        /// Creates a new instance of a <see cref="IFormField"/>.
+        /// </summary>
+        /// <param name="settings">The current form settings.</param>
+        /// <returns>A <see cref="IFormField"/>.</returns>
         IFormField CreateField(IFormFieldSettings settings);
     }
 }
