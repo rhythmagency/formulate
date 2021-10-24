f6e0925bd143441ba106bf0c86f639709be8b996
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-23 21:34:32 -0400

    [v4] Added JsonUtility.

---
+++ src/Formulate.Core/Utilities/IJsonUtility.cs
@@ -0,0 +1,23 @@
+﻿namespace Formulate.Core.Utilities
+{
+    /// <summary>
+    /// A utility that handles JSON serialization.
+    /// </summary>
+    public interface IJsonUtility
+    {
+        /// <summary>
+        /// Deserialize a string to a given type.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize to.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>A <typeparamref name="T"/>.</returns>
+        T Deserialize<T>(string value);
+
+        /// <summary>
+        /// Serialize a object to a JSON string.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>A <see cref="string"/>.</returns>
+        string Serialize(object value);
+    }
+}
---
+++ src/Formulate.Core/Utilities/Internal/SystemTextJsonUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Formulate.Core.Utilities.Internal
+{
+    /// <summary>
+    /// An implementation of <see cref="IJsonUtility"/> that uses <see cref="JsonSerializer"/>.
+    /// </summary>
+    internal sealed class SystemTextJsonUtility : IJsonUtility
+    {
+        /// <inheritdoc />
+        public T Deserialize<T>(string value)
+        {
+            return string.IsNullOrWhiteSpace(value) ? default : JsonSerializer.Deserialize<T>(value);
+        }
+
+        /// <inheritdoc />
+        public string Serialize(object value)
+        {
+            return JsonSerializer.Serialize(value);
+        }
+    }
+}
