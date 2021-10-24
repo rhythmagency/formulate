80cb42173f049970111b4bc6886fe568eabd9433
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-23 20:00:29 -0400

    [v4] Added simple text field.

---
+++ src/Formulate.Core/FormFields/Text/TextField.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Formulate.Core.Validations;
+
+namespace Formulate.Core.FormFields.Text
+{
+    /// <summary>
+    /// A text field.
+    /// </summary>
+    public sealed class TextField : FormField
+    {
+        /// <inheritdoc />
+        public TextField(IFormFieldSettings settings) : base(settings)
+        {
+        }
+
+        /// <inheritdoc />
+        public TextField(IFormFieldSettings settings, IReadOnlyCollection<IValidation> validations) : base(settings, validations)
+        {
+        }
+    }
+}
---
+++ src/Formulate.Core/FormFields/Text/TextFieldType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Formulate.Core.FormFields.Text
+{
+    /// <summary>
+    /// A text form field type.
+    /// </summary>
+    public sealed class TextFieldType : FormFieldType
+    {
+        /// <summary>
+        /// Constants related to <see cref="TextFieldType"/>.
+        /// </summary>
+        public static class Constants
+        {
+            /// <summary>
+            /// The type id.
+            /// </summary>
+            public const string TypeId = "1790658086EA440BBC309E1B099F803B";
+
+            /// <summary>
+            /// The type label.
+            /// </summary>
+            public const string TypeLabel = "Text";
+
+            /// <summary>
+            /// The icon.
+            /// </summary>
+            public const string Icon = "icon-document-dashed-line";
+
+            /// <summary>
+            /// The Angular JS directive.
+            /// </summary>
+            public const string Directive = "formulate-text-field";
+        }
+
+        /// <inheritdoc />
+        public override string Directive => Constants.Directive;
+
+        /// <inheritdoc />
+        public override string TypeLabel => Constants.TypeLabel;
+
+        /// <inheritdoc />
+        public override string Icon => Constants.Icon;
+
+        /// <inheritdoc />
+        public override Guid TypeId => new Guid(Constants.TypeId);
+
+        /// <inheritdoc />
+        public override IFormField CreateField(IFormFieldSettings settings)
+        {
+            return new TextField(settings);
+        }
+    }
+}
