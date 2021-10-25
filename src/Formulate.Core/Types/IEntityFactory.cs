e9cf61d1b06bbd802d8722f52cc0fd27b0501368
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-24 22:20:19 -0400

    [v4] Created IEntityFactory to consolidate the patter for creating an entity from settings.

---
+++ src/Formulate.Core/Types/IEntityFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// A contract for creating a factory which convert <typeparamref name="TSettings"/> into a <typeparamref name="TEntity"/>.
+    /// </summary>
+    /// <typeparam name="TSettings">The type of the settings.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <remarks>This should only be used for readonly operations not persistence.</remarks>
+    public interface IEntityFactory<in TSettings, out TEntity> where TSettings : IEntitySettings where TEntity : IEntity
+    {
+        /// <summary>
+        /// Creates a new instance which implements <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="settings">The current settings.</param>
+        /// <returns>A <typeparamref name="TEntity"/>.</returns>
+        TEntity Create(TSettings settings);
+    }
+}
