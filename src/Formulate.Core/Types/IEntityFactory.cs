6f5f57c8ea2f3a2f945c4f1178ff8380d8756682
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-24 22:26:06 -0400

    [v4] typo in xml-docs in entityfactory.

--- src/Formulate.Core/Types/IEntityFactory.cs
+++ src/Formulate.Core/Types/IEntityFactory.cs
@@ -1,7 +1,7 @@
 ﻿namespace Formulate.Core.Types
 {
     /// <summary>
-    /// A contract for creating a factory which convert <typeparamref name="TSettings"/> into a <typeparamref name="TEntity"/>.
+    /// A contract for creating a factory which converts <typeparamref name="TSettings"/> into a <typeparamref name="TEntity"/>.
     /// </summary>
     /// <typeparam name="TSettings">The type of the settings.</typeparam>
     /// <typeparam name="TEntity">The type of the entity.</typeparam>
