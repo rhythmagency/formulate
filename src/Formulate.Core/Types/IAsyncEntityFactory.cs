37a0ff4db2c8bac522015111d6b6a9d95a4a7d11
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-24 22:29:04 -0400

    [v4] Added async version of IEntityFactory.

---
+++ src/Formulate.Core/Types/IAsyncEntityFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// A contract for creating a factory which asynchronously creates a <typeparamref name="TEntity"/> from a <typeparamref name="TSettings"/>.
+    /// </summary>
+    /// <typeparam name="TSettings">The type of the settings.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <remarks>This should only be used for readonly operations not persistence.</remarks>
+    public interface IAsyncEntityFactory<in TSettings, TEntity> where TSettings : IEntitySettings where TEntity : IEntity
+    {
+        /// <summary>
+        /// Asynchronously creates a new instance which implements <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <param name="settings">The current settings.</param>
+        /// <returns>A <typeparamref name="TEntity"/>.</returns>
+        Task<TEntity> CreateAsync(TSettings settings, CancellationToken cancellationToken = default);
+    }
+}
