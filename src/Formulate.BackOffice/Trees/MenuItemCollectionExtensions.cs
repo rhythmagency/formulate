ed41460df90ac4ae22dcbcc138a33dae8d02f8ae
jamiepollock <jamie.does.umbraco@gmail.com>
2021-10-28 19:22:37 -0400

    [v4] Added extension methods for menu item collection.

---
+++ src/Formulate.BackOffice/Trees/MenuItemCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Umbraco.Cms.Core.Models.Trees;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Trees;
+
+namespace Formulate.BackOffice.Trees
+{
+    /// <summary>
+    /// Extension methods that augments the <see cref="MenuItemCollection"/> class.
+    /// </summary>
+    internal static partial class MenuItemCollectionExtensions
+    {
+        /// <summary>
+        /// Add a refresh menu item to the menu.
+        /// </summary>
+        /// <param name="menuItemCollection">The current menu item collection.</param>
+        /// <param name="localizedTextService">The localized text service.</param>
+        /// <param name="separatorBefore">Determines if we add a separator before this menu item.</param>
+        public static void AddRefreshMenuItem(this MenuItemCollection menuItemCollection,
+            ILocalizedTextService localizedTextService, bool separatorBefore = true)
+        {
+            menuItemCollection.Items.Add(new RefreshNode(localizedTextService, separatorBefore));
+        }
+    }
+}
