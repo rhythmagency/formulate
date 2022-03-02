b25886724aed8baad9d34e2997d4cae7850dd836
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-03-02 07:53:27 -0800

    feat (Build) create bat file to update frontend in sample website

---
+++ build/update-frontend.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM This file allows you to update the frontend assets without interrupting the current Visual Studio build.
+
+cd ../src/Website/
+
+START /WAIT /B ../CustomBuildActions/bin/debug/net6.0/CustomBuildActions.exe -generate-package-manifest -copy-static-assets-to-website
