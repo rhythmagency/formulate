9ccc2b7ae98af9340b51b0c9b80f336fcf972836
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-03-02 09:22:24 -0800

    chore (Whitespace) use file-scoped namespaces to reduce indentation

--- src/CustomBuildActions/CopyStaticAssetsToWebsite.cs
+++ src/CustomBuildActions/CopyStaticAssetsToWebsite.cs
@@ -1,72 +1,71 @@
-﻿namespace CustomBuildActions
+﻿namespace CustomBuildActions;
+
+/// <summary>
+/// Copies the static assets to the website.
+/// </summary>
+internal class CopyStaticAssetsToWebsite
 {
     /// <summary>
     /// Copies the static assets to the website.
     /// </summary>
-    internal class CopyStaticAssetsToWebsite
+    public static void Copy()
     {
-        /// <summary>
-        /// Copies the static assets to the website.
-        /// </summary>
-        public static void Copy()
+        // Variables.
+        var source = NormalizePath("../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice");
+        var destination = NormalizePath("../Website/App_Plugins/FormulateBackOffice");
+
+        // Clear out the old directory first.
+        if (Directory.Exists(destination))
         {
-            // Variables.
-            var source = NormalizePath("../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice");
-            var destination = NormalizePath("../Website/App_Plugins/FormulateBackOffice");
-
-            // Clear out the old directory first.
-            if (Directory.Exists(destination))
-            {
-                Directory.Delete(destination, true);
-            }
-
-            /// Copy the files.
-            CopyDirectory(source, destination);
+            Directory.Delete(destination, true);
         }
 
-        /// <summary>
-        /// Copies the source directory to the target directory.
-        /// </summary>
-        /// <param name="source">
-        /// The source directory.
-        /// </param>
-        /// <param name="destination">
-        /// The destination directory.
-        /// </param>
-        private static void CopyDirectory(string source, string destination)
+        /// Copy the files.
+        CopyDirectory(source, destination);
+    }
+
+    /// <summary>
+    /// Copies the source directory to the target directory.
+    /// </summary>
+    /// <param name="source">
+    /// The source directory.
+    /// </param>
+    /// <param name="destination">
+    /// The destination directory.
+    /// </param>
+    private static void CopyDirectory(string source, string destination)
+    {
+        // Normalize paths.
+        source = NormalizePath(source);
+        destination = NormalizePath(destination);
+
+        // Ensure the destination directories exist.
+        foreach(var path in Directory.GetDirectories(source, "*", SearchOption.AllDirectories))
         {
-            // Normalize paths.
-            source = NormalizePath(source);
-            destination = NormalizePath(destination);
-
-            // Ensure the destination directories exist.
-            foreach(var path in Directory.GetDirectories(source, "*", SearchOption.AllDirectories))
-            {
-                Directory.CreateDirectory(path.Replace(source, destination));
-            }
-
-            // Copy all the files.
-            foreach(var path in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
-            {
-                File.Copy(path, path.Replace(source, destination), true);
-            }
+            Directory.CreateDirectory(path.Replace(source, destination));
         }
 
-        /// <summary>
-        /// Normalizes the path to ensure it uses a standard format (backslash
-        /// separator, no trailing slash, no dot directories).
-        /// </summary>
-        /// <param name="path">
-        /// The unnormalized path.
-        /// </param>
-        /// <returns>
-        /// The normalized path.
-        /// </returns>
-        private static string NormalizePath(string path)
+        // Copy all the files.
+        foreach(var path in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
         {
-            return Path.GetFullPath(path)
-                .Replace(@"/", @"\")
-                .TrimEnd(@"\".ToCharArray());
+            File.Copy(path, path.Replace(source, destination), true);
         }
+    }
+
+    /// <summary>
+    /// Normalizes the path to ensure it uses a standard format (backslash
+    /// separator, no trailing slash, no dot directories).
+    /// </summary>
+    /// <param name="path">
+    /// The unnormalized path.
+    /// </param>
+    /// <returns>
+    /// The normalized path.
+    /// </returns>
+    private static string NormalizePath(string path)
+    {
+        return Path.GetFullPath(path)
+            .Replace(@"/", @"\")
+            .TrimEnd(@"\".ToCharArray());
     }
 }
--- src/CustomBuildActions/GeneratePackageManifest.cs
+++ src/CustomBuildActions/GeneratePackageManifest.cs
@@ -1,42 +1,41 @@
-﻿using System.Text.Json;
+﻿namespace CustomBuildActions;
 
-namespace CustomBuildActions
+using System.Text.Json;
+
+/// <summary>
+/// Generates the package.manifest.
+/// </summary>
+internal class GeneratePackageManifest
 {
     /// <summary>
-    /// Generates the package.manifest.
+    /// Generates the package.manifest that contains the frontend resources used
+    /// by Formulate in the Umbraco back office.
     /// </summary>
-    internal class GeneratePackageManifest
+    public static void Generate()
     {
-        /// <summary>
-        /// Generates the package.manifest that contains the frontend resources used
-        /// by Formulate in the Umbraco back office.
-        /// </summary>
-        public static void Generate()
+        // Variables.
+        var searchPattern = "*.js";
+        var rootFolder = "../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/";
+        var allFolders = SearchOption.AllDirectories;
+        var manifestPath = "../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/package.manifest";
+
+        // Get all the files, then format their paths.
+        var files = Directory.GetFiles(rootFolder, searchPattern, allFolders)
+            .Select(x => x.Substring(36))
+            .Select(x => x.Replace(@"\", @"/"));
+
+        // Serialize the JSON stores in the package.manifest file.
+        var contents = new
         {
-            // Variables.
-            var searchPattern = "*.js";
-            var rootFolder = "../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/";
-            var allFolders = SearchOption.AllDirectories;
-            var manifestPath = "../Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/package.manifest";
+            javascript = files,
+        };
+        var options = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+        };
+        var serialized = JsonSerializer.Serialize(contents, options);
 
-            // Get all the files, then format their paths.
-            var files = Directory.GetFiles(rootFolder, searchPattern, allFolders)
-                .Select(x => x.Substring(36))
-                .Select(x => x.Replace(@"\", @"/"));
-
-            // Serialize the JSON stores in the package.manifest file.
-            var contents = new
-            {
-                javascript = files,
-            };
-            var options = new JsonSerializerOptions()
-            {
-                WriteIndented = true,
-            };
-            var serialized = JsonSerializer.Serialize(contents, options);
-
-            // Store the JSON to the package.manifest file.
-            File.WriteAllText(manifestPath, serialized);
-        }
+        // Store the JSON to the package.manifest file.
+        File.WriteAllText(manifestPath, serialized);
     }
 }
