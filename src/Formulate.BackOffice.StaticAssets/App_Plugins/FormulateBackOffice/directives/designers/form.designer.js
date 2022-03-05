a59b807f299bc704abbc616f9d986f7f21254231
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-03-04 17:22:35 -0800

    feat (Forms) add the toggle/delete functions

--- src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/form.designer.js
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/form.designer.js
@@ -96,10 +96,92 @@ function formulateFormDesignerDirective() {
                 scope.apps[0].active = true;
                 scope.appChanged(scope.apps[0]);
             }
+
+            scope.events = new FormDesignerEventHandlers({
+                $scope: scope,
+            });
         }
     };
 
     return directive;
 }
+
+/**
+ * The event handlers for the form designer.
+ */
+class FormDesignerEventHandlers {
+
+    // Properties.
+    $scope;
+
+    /**
+     * Constructor.
+     * @param services The services the event handlers will need.
+     */
+    constructor(services) {
+        Object.keys(services).forEach((x) => this[x] = services[x]);
+    }
+
+    /**
+     * Toggles the field to either show or hide it.
+     * @param field The field to show or hide.
+     */
+    toggleField = (field) => {
+        field.expanded = !field.expanded;
+    };
+
+    /**
+     * Deletes the specified field (after confirming with the user).
+     * @param field The field to delete.
+     */
+    deleteField = (field) => {
+
+        // Confirm deletion.
+        const name = field.name === null || !field.name.length
+            ? "unnamed field"
+            : `field, "${field.name}"`;
+        const message = `Are you sure you wanted to delete the ${name}?`;
+        const response = confirm(message);
+
+        // Delete field?
+        if (response) {
+            const index = this.$scope.fields.indexOf(field);
+            this.$scope.fields.splice(index, 1);
+        }
+
+    };
+
+    /**
+     * Toggles the handler to either show or hide it.
+     * @param handler The handler to toggle.
+     */
+    toggleHandler = (handler) => {
+        handler.expanded = !handler.expanded;
+        //TODO: I think we need a corresponding CSS rule for this to work fully.
+    };
+
+    /**
+     * Deletes the specified handler (after confirming with the user).
+     * @param handler The handler to delete.
+     */
+    deleteHandler = (handler) => {
+
+        // Confirm deletion.
+        const name = handler.name === null || !handler.name.length
+            ? "unnamed handler"
+            : `handler, "${handler.name}"`;
+        const message = `Are you sure you wanted to delete the ${name}?`;
+        const response = confirm(message);
+
+        // Delete handler?
+        if (response) {
+            const index = this.$scope.handlers.indexOf(handler);
+            this.$scope.handlers.splice(index, 1);
+        }
+
+    };
+
+}
+
 
 angular.module("umbraco.directives").directive("formulateFormDesignerV2", formulateFormDesignerDirective);
