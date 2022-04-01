0d70bf267a763c59e2942b5a5d454920bc9b3b58
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-04-01 16:19:04 -0700

    feat (Configured Forms) add shell for configured form designer

---
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/configured-form.designer.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    function formulateConfiguredFormDesignerDirective(
+            $http, $location, $routeParams, formulateDefinitionDirectiveResource,
+            navigationService, notificationsService, formHelper) {
+        const directive = {
+            replace: true,
+            templateUrl: "/app_plugins/formulatebackoffice/directives/designers/configured-form.designer.html",
+            scope: {
+                entity: "=",
+            },
+            link: function (scope, element, attrs) {
+                //TODO: ...
+            },
+        };
+
+        return directive;
+    }
+
+    angular
+        .module("umbraco.directives")
+        .directive("formulateConfiguredFormDesigner", formulateConfiguredFormDesignerDirective);
+})();
