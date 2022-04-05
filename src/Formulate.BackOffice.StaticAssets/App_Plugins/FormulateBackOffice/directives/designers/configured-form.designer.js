1fff932405a661ad80d61bc085a0a438134f3a48
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-04-05 13:29:54 -0700

    feat (Configured Forms) properly manage the state of the save button

--- src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/configured-form.designer.js
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/configured-form.designer.js
@@ -2,24 +2,28 @@
     function formulateConfiguredFormDesignerDirective(
             notificationsService, formHelper, overlayService,
             formulateTypeDefinitionResource) {
+
+        const link = (scope) => {
+            scope.saveButtonState = 'init';
+            const services = {
+                $scope: scope,
+                overlayService,
+            };
+            scope.events = new ConfiguredFormEvents(services);
+
+            initializeTemplates(formulateTypeDefinitionResource, scope);
+        };
+
         const directive = {
             replace: true,
             templateUrl: "/app_plugins/formulatebackoffice/directives/designers/configured-form.designer.html",
             scope: {
                 entity: "=",
             },
-            link: function (scope, element, attrs) {
-                const services = {
-                    $scope: scope,
-                    overlayService,
-                };
-                scope.events = new ConfiguredFormEvents(services);
-
-                initializeTemplates(formulateTypeDefinitionResource, scope);
-            },
+            link: link,
         };
 
-        return directive;
+        return (directive);
     }
 
     /**
@@ -33,6 +37,7 @@
         formulateTypeDefinitionResource.getTemplateDefinitions()
             .then((data) => {
                 scope.templates = data;
+                scope.initialized = true;
             });
     }
 
@@ -83,6 +88,31 @@
             // Open the overlay that displays the layouts.
             this.overlayService.open(data);
 
+        };
+
+        /**
+         * Can the configured form be saved currently?
+         * @returns {boolean} True, if the configured form can be saved; otherwise, false.
+         */
+        canSave = () => {
+            return this.$scope.initialized && this.isReadyToSave() && this.hasValidName();
+        };
+
+        /**
+         * Is the configured form currently in a state which is not busy?
+         * @returns {boolean} True, if the configured form is in a state that is ready to save;
+         *      otherwise, false.
+         */
+        isReadyToSave = () => {
+            return this.$scope.saveButtonState !== 'busy';
+        };
+
+        /**
+         * Does the configured form have a valid name?
+         * @returns {boolean} True, if the configured form has a valid name; otherwise, false.
+         */
+        hasValidName = () => {
+            return this.$scope.entity.name && this.$scope.entity.name.length > 0;
         };
 
     }
