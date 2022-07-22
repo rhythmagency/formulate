3497c472d547d71ddc4604e26584112c5e937732
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-07-22 10:20:16 -0700

    feat (Form Picker) WIP: implement form picker property editor

--- src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/property-editors/configured-form-picker/form-picker.js
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/property-editors/configured-form-picker/form-picker.js
@@ -5,18 +5,16 @@ class ConfiguredFormPicker {
     constructor() {
     }
 
-    controller = ($scope) => {
+    controller = ($scope, overlayService) => {
         this.retainProperties({
             $scope,
+            overlayService,
         });
     };
 
     retainProperties = (properties) => {
         for (const [key, value] of Object.entries(properties)) {
             this[key] = value;
-            console.log('Stored', {
-                key, value
-            });
         }
     };
 
@@ -24,6 +22,41 @@ class ConfiguredFormPicker {
         angular
             .module('umbraco')
             .controller('Formulate.ConfiguredFormPicker', this.controller);
+    };
+
+    /**
+     * Opens the form chooser dialog.
+     */
+    pickConfiguredForm = () => {
+
+        // This is called when the dialog is closed.
+        const closer = () => {
+            this.overlayService.close();
+        };
+
+        // This is called when a form is chosen.
+        const chosen = ({ id, name }) => {
+            this.overlayService.close();
+            this.$scope.model.value = {
+                id,
+            };
+            this.$scope.formName = name;
+        };
+
+        // The data sent to the layout chooser.
+        const data = {
+            title: "Choose Form",
+            subtitle: "Choose a form.",
+            //TODO: Need to implement this view. Refer to the layout chooser.
+            view: "/app_plugins/formulatebackoffice/directives/overlays/configured-form-chooser/form-chooser-overlay.html",
+            hideSubmitButton: true,
+            close: closer,
+            chosen: chosen,
+        };
+
+        // Open the overlay that displays the forms.
+        this.overlayService.open(data);
+
     };
 }
 
