4b23f2f0f573332d590a98da5be81db9888fe2ba
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-07-22 08:45:55 -0700

    feat (Namespaces) try file-level namespaces in a couple files

--- src/Website/Program.cs
+++ src/Website/Program.cs
@@ -1,22 +1,21 @@
+namespace Website;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Website
+public class Program
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args)
-                .Build()
-                .Run();
+    public static void Main(string[] args)
+        => CreateHostBuilder(args)
+            .Build()
+            .Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureUmbracoDefaults()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureUmbracoDefaults()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStaticWebAssets();
+                webBuilder.UseStartup<Startup>();
+            });
 }
--- src/Website/Startup.cs
+++ src/Website/Startup.cs
@@ -1,3 +1,5 @@
+namespace Website;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -7,71 +9,68 @@ using System;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Extensions;
 
-namespace Website
+public class Startup
 {
-    public class Startup
+    private readonly IWebHostEnvironment _env;
+    private readonly IConfiguration _config;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Startup" /> class.
+    /// </summary>
+    /// <param name="webHostEnvironment">The web hosting environment.</param>
+    /// <param name="config">The configuration.</param>
+    /// <remarks>
+    /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+    /// </remarks>
+    public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
     {
-        private readonly IWebHostEnvironment _env;
-        private readonly IConfiguration _config;
+        _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup" /> class.
-        /// </summary>
-        /// <param name="webHostEnvironment">The web hosting environment.</param>
-        /// <param name="config">The configuration.</param>
-        /// <remarks>
-        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
-        /// </remarks>
-        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
-        {
-            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
-            _config = config ?? throw new ArgumentNullException(nameof(config));
-        }
-
-        /// <summary>
-        /// Configures the services.
-        /// </summary>
-        /// <param name="services">The services.</param>
-        /// <remarks>
-        /// This method gets called by the runtime. Use this method to add services to the container.
-        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        /// </remarks>
-        public void ConfigureServices(IServiceCollection services)
-        {
+    /// <summary>
+    /// Configures the services.
+    /// </summary>
+    /// <param name="services">The services.</param>
+    /// <remarks>
+    /// This method gets called by the runtime. Use this method to add services to the container.
+    /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    /// </remarks>
+    public void ConfigureServices(IServiceCollection services)
+    {
 #pragma warning disable IDE0022 // Use expression body for methods
-            services.AddUmbraco(_env, _config)
-                .AddBackOffice()
-                .AddWebsite()
-                .AddComposers()
-                .Build();
+        services.AddUmbraco(_env, _config)
+            .AddBackOffice()
+            .AddWebsite()
+            .AddComposers()
+            .Build();
 #pragma warning restore IDE0022 // Use expression body for methods
 
-        }
+    }
 
-        /// <summary>
-        /// Configures the application.
-        /// </summary>
-        /// <param name="app">The application builder.</param>
-        /// <param name="env">The web hosting environment.</param>
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    /// <summary>
+    /// Configures the application.
+    /// </summary>
+    /// <param name="app">The application builder.</param>
+    /// <param name="env">The web hosting environment.</param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseUmbraco()
-                .WithMiddleware(u =>
-                {
-                    u.UseBackOffice();
-                    u.UseWebsite();
-                })
-                .WithEndpoints(u =>
-                {
-                    u.UseInstallerEndpoints();
-                    u.UseBackOfficeEndpoints();
-                    u.UseWebsiteEndpoints();
-                });
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseUmbraco()
+            .WithMiddleware(u =>
+            {
+                u.UseBackOffice();
+                u.UseWebsite();
+            })
+            .WithEndpoints(u =>
+            {
+                u.UseInstallerEndpoints();
+                u.UseBackOfficeEndpoints();
+                u.UseWebsiteEndpoints();
+            });
     }
 }
