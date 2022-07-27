13a991212663c83f1433fbb9004a7c39fe1f4451
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2022-07-26 18:16:46 -0700

    feat (Layout Designer) WIP: implement layout designer

---
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/designers/layout.designer.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    function formulateLayoutDesignerDirective(
+        $http,
+        $location,
+        $routeParams,
+        formulateDefinitionDirectiveResource,
+        navigationService,
+        notificationsService,
+        formHelper) {
+        var directive = {
+            replace: true,
+            templateUrl: "/app_plugins/formulatebackoffice/directives/designers/layout.designer.html",
+            scope: {
+                entity: "=",
+            },
+            link: (scope) => {
+                scope.test = JSON.parse(scope.entity.data);
+            },
+        };
+
+        return directive;
+    }
+
+    angular.module("umbraco.directives")
+        .directive("formulateLayoutDesigner",
+            formulateLayoutDesignerDirective);
+})();
