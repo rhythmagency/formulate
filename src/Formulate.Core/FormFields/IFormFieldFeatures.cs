0353e4303ccda16eff68fb66120e5a297ca8530c
jamiepollock <jamie.does.umbraco@gmail.com>
2022-07-28 13:05:53 -0400

    Added IFormFieldFeatures
    This is an interface to take the properties of a form field definition and separate them from the CreateField method.

---
+++ src/Formulate.Core/FormFields/IFormFieldFeatures.cs
@@ -0,0 +1,29 @@
+﻿namespace Formulate.Core.FormFields
+{
+    /// <summary>
+    /// A contract to implement features for a Form Field.
+    /// </summary>
+    /// <remarks>Any feature should be readonly and implemented inline.</remarks>
+    public interface IFormFieldFeatures
+    {
+        /// <summary>
+        /// Gets a value indicating whether this field definition is transitory.
+        /// </summary>
+        bool IsTransitory { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field definition is server side only.
+        /// </summary>
+        bool IsServerSideOnly { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field definition is hidden.
+        /// </summary>
+        bool IsHidden { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether this field definition is stored.
+        /// </summary>
+        bool IsStored { get; }
+    }
+}
