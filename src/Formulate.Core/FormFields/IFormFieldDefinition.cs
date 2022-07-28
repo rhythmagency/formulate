85ae9a2804aa2a353f1d2d76d374d9e1da4e885d
jamiepollock <jamie.does.umbraco@gmail.com>
2022-07-28 13:06:29 -0400

    Added IFormFieldFeatures to IFormFieldDefinition

--- src/Formulate.Core/FormFields/IFormFieldDefinition.cs
+++ src/Formulate.Core/FormFields/IFormFieldDefinition.cs
@@ -6,32 +6,12 @@
     /// <summary>
     /// A contract for implementing a form field definition.
     /// </summary>
-    public interface IFormFieldDefinition : IDefinition
+    public interface IFormFieldDefinition : IDefinition, IFormFieldFeatures
     {
         /// <summary>
         /// Gets the icon.
         /// </summary>
         string Icon { get; }
-        
-        /// <summary>
-        /// Gets a value indicating whether this field definition is transitory.
-        /// </summary>
-        bool IsTransitory { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether this field definition is server side only.
-        /// </summary>
-        bool IsServerSideOnly { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether this field definition is hidden.
-        /// </summary>
-        bool IsHidden { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether this field definition is stored.
-        /// </summary>
-        bool IsStored { get; }
 
         /// <summary>
         /// Creates a new instance of a <see cref="FormField"/>.
