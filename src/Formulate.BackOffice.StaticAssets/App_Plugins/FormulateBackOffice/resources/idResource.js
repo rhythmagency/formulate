2af26308a1c55a09e4d3ab56e8fb8eee7df51c2f
jamiepollock <jamie.does.umbraco@gmail.com>
2022-07-29 13:29:07 -0400

    Added formulateIds (see description)
    This is to handle any discrepancies between Guid format d and n which appears to be inconsistent throughout the app.
    
    Also contains a generateId function and sanitize too.

---
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/resources/idResource.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    // Variables.
+    var app = angular.module("umbraco");
+
+    app.factory("formulateIds", function () {
+        const sanitizeRegex = /-/g;
+
+        return {
+            compare: compare,
+            generateId: generateId,
+            sanitize: sanitize,
+        };
+
+        function compare(a, b) {
+            if (a === b) {
+                return true;
+            }
+
+            if (typeof (a) === 'undefined') {
+                return false;
+            }
+
+            if (typeof (b) === 'undefined') {
+                return false;
+            }
+
+            return sanitize(a) === sanitize(b);
+        }
+
+        function generateId() {
+            return crypto.randomUUID();
+        }
+
+        function sanitize(id) {
+            if (!id) {
+                return '';
+            }
+
+            return id.trim().toLowerCase().replace(sanitizeRegex, '');
+        }
+    });
+})();
