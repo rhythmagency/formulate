7314adb3fdf3eb56aa8fb20aba164c5ab0817bc2
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-09 16:42:01 -0400

    Deleted unused request/response classes.

--- src/Formulate.BackOffice/Controllers/DataValues/SavePersistedValidationResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Formulate.BackOffice.Controllers.DataValues
-{
-    public class SavePersistedDataValuesResponse
-    {
-        public string EntityId { get; set; }
-        public string[] EntityPath { get; set; }
-    }
-}
