cba33dd1ba4fc80f05ad8486d08a68e25e466286
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-09 17:50:20 -0400

    Added Newtonsoft Json Serializer.

---
+++ src/Formulate.Core/Utilities/Internal/NewtonsoftJsonSerializer.cs
@@ -0,0 +1,23 @@
+﻿namespace Formulate.Core.Utilities.Internal
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// An implementation of <see cref="IJsonSerializer"/> that uses <see cref="JsonConvert"/>.
+    /// </summary>
+    internal sealed class NewtonsoftJsonSerializer : IJsonSerializer
+    {
+        /// <inheritdoc />
+        public T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        /// <inheritdoc />
+        public string Serialize(object value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented);
+        }
+    }
+}
