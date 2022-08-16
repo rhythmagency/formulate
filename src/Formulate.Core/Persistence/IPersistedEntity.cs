c50be1c88da1016e7c9955f492218be1b32a7d16
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-16 14:40:32 -0400

    IPersistedEntity now inherits IPersistedItem.

--- src/Formulate.Core/Persistence/IPersistedEntity.cs
+++ src/Formulate.Core/Persistence/IPersistedEntity.cs
@@ -5,26 +5,11 @@ namespace Formulate.Core.Persistence
     /// <summary>
     /// A contract for creating a persisted entity.
     /// </summary>
-    public interface IPersistedEntity
+    public interface IPersistedEntity : IPersistedItem
     {
-        /// <summary>
-        /// Gets the ID.
-        /// </summary>
-        Guid Id { get; }
-
         /// <summary>
         /// Gets the path.
         /// </summary>
         Guid[] Path { get; }
-
-        /// <summary>
-        /// Gets the name.
-        /// </summary>
-        string Name { get; }
-
-        /// <summary>
-        /// Gets the alias.
-        /// </summary>
-        string Alias { get; }
     }
 }
