a6bfec1d096d3db5b24aed7a81eb551567672359
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-16 14:46:56 -0400

    Added MapperContextExtensions for augmenting MapperContext

---
+++ src/Formulate.BackOffice/Mapping/EditorModels/MapperContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Formulate.BackOffice.Mapping.EditorModels
+{
+    using Umbraco.Cms.Core.Mapping;
+
+    internal static class MapperContextExtensions
+    {
+        public static void SetIsNew(this MapperContext mapperContext, bool value)
+        {
+            mapperContext.Items.Add("isNew", value);
+        }
+
+        public static bool IsNew(this MapperContext mapperContext)
+        {
+            if (mapperContext.Items.TryGetValue("isNew", out var value) && value is bool isNew)
+            {
+                return isNew;
+            }
+
+            return false;
+        }
+    }
+}
