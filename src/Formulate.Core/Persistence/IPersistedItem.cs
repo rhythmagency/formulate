7e59303b8b73bcf46f8fd86afdc51bc9c5b5a5c6
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-16 14:40:13 -0400

    Added IPersistedItem.

---
+++ src/Formulate.Core/Persistence/IPersistedItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Formulate.Core.Persistence
+{
+    /// <summary>
+    /// A contract for creating a persisted item.
+    /// </summary>
+    public interface IPersistedItem
+    {
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the alias.
+        /// </summary>
+        string Alias { get; }
+    }
+}
