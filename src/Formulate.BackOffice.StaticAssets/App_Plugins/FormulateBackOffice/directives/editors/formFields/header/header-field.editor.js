585d3a62d046ae2f0be2f1ae4d3f6674ec4ef2c4
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-17 14:58:04 -0400

    Added editor for Header Field config.

---
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/editors/formFields/header/header-field.editor.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+(function () {
+    // Controller.
+    function controller($scope) {
+
+
+    }
+
+    // Directive.
+    function directive() {
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: "/App_Plugins/FormulateBackOffice/directives/editors/formFields/header/header-field.editor.html",
+            controller: controller,
+            scope: {
+                configuration: "="
+            }
+        };
+    }
+
+    // Associate directive/controller.
+    angular.module("umbraco").directive("formulateHeaderField", directive);
+
+})();
