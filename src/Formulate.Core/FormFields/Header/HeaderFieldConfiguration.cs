1c4c84e53acd86a5757c8fc476de4c0f119bb211
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-17 14:57:08 -0400

    Added HeaderFieldConfiguration.

---
+++ src/Formulate.Core/FormFields/Header/HeaderFieldConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Formulate.Core.FormFields.Header
+{
+    using System.Runtime.Serialization;
+    
+    /// <summary>
+    /// Configuation required by <see cref="HeaderField"/>.
+    /// </summary>
+    [DataContract]
+    public sealed class HeaderFieldConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        [DataMember(Name = "text")]
+        public string Text { get; set; }
+    }
+}
