354b06bcdeeae747f20b6729593af703782988e7
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-18 17:40:35 -0400

    Create Constants-Icons.cs

---
+++ src/Formulate.BackOffice/Constants-Icons.cs
@@ -0,0 +1,88 @@
+﻿namespace Formulate.BackOffice
+{
+    public static partial class Constants
+    {
+        /// <summary>
+        /// Constants related to Icons.
+        /// </summary>
+        public static class Icons
+        {
+            /// <summary>
+            /// Constants related to tree root icons.
+            /// </summary>
+            public static class TreeRoots
+            {                
+                /// <summary>
+                /// The DataValues tree root icon.
+                /// </summary>
+                public const string DataValues = "icon-formulate-values";
+
+                /// <summary>
+                /// The Forms tree root icon.
+                /// </summary>
+                public const string Forms = "icon-formulate-forms";
+
+                /// <summary>
+                /// The Layouts tree root icon.
+                /// </summary>
+                public const string Layouts = "icon-formulate-layouts";
+
+                /// <summary>
+                /// The Validations tree root icon.
+                /// </summary>
+                public const string Validations = "icon-formulate-validations";
+            }
+
+            /// <summary>
+            /// Constants related to folder icons.
+            /// </summary>
+            public static class Folders
+            {
+                /// <summary>
+                /// The DataValues folder icon.
+                /// </summary>
+                public const string DataValues = "icon-formulate-value-group";
+
+                /// <summary>
+                /// The Forms folder icon.
+                /// </summary>
+                public const string Forms = "icon-formulate-form-group";
+
+                /// <summary>
+                /// The Layouts folder icon.
+                /// </summary>
+                public const string Layouts = "icon-formulate-layout-group";
+
+                /// <summary>
+                /// The Validations folder icon.
+                /// </summary>
+                public const string Validations = "icon-formulate-validation-group";
+            }
+
+            /// <summary>
+            /// The default DataValues icon.
+            /// </summary>
+            public const string DataValues = "icon-formulate-value";
+
+            /// <summary>
+            /// The configured form icon.
+            /// </summary>
+            public const string ConfiguredForm = "icon-formulate-conform";
+
+            /// <summary>
+            /// The form icon.
+            /// </summary>
+            public const string Form = "icon-formulate-form";
+
+            /// <summary>
+            /// The layout icon.
+            /// </summary>
+            public const string Layout = "icon-formulate-layout";
+
+            /// <summary>
+            /// The validation icon.
+            /// </summary>
+            public const string Validation = "icon-formulate-validation";
+        }
+    }
+}
