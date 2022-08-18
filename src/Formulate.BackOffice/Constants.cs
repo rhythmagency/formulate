413b8230d40e19de7681e6b51c642f611d087015
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-18 17:40:26 -0400

    Added initial Constants.cs

---
+++ src/Formulate.BackOffice/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Formulate.BackOffice
+{
+    /// <summary>
+    /// A root class for various constants related to the Formulate BackOffice.
+    /// </summary>
+    public static partial class Constants
+    {
+    }
+}
