420e59aa2edcd978ca41762cb7059dd155e15bd9
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-18 10:40:08 -0400

    Removed DataContract/Member from FormHandlerDefinition and FormFieldDefinition
    These classes are now longer used in serialization.

--- src/Formulate.Core/FormFields/FormFieldDefinitionBase.cs
+++ src/Formulate.Core/FormFields/FormFieldDefinitionBase.cs
@@ -8,58 +8,46 @@
     /// An abstract class for creating a new <see cref="IFormFieldDefinition"/>.
     /// </summary>
     /// <remarks>It is not necessary to implement from this class but it does provide some helpful defaults for certain optional fields.</remarks>
-    [DataContract]
     public abstract class FormFieldDefinitionBase<TField> : IFormFieldDefinition
         where TField : IFormField
     {
         /// <inheritdoc />
-        [DataMember(Name = "kindId")]
         public abstract Guid KindId { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "definitionLabel")]
         public abstract string DefinitionLabel { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "icon")]
         public abstract string Icon { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "directive")]
         public abstract string Directive { get; }
 
         /// <inheritdoc />
         /// <remarks>Defaults to false.</remarks>
-        [DataMember(Name = "isTransitory")]
         public virtual bool IsTransitory => false;
 
         /// <inheritdoc />
         /// <remarks>Defaults to false.</remarks>
-        [DataMember(Name = "isServerSideOnly")]
         public virtual bool IsServerSideOnly => false;
 
         /// <inheritdoc />
         /// <remarks>Defaults to false.</remarks>
-        [DataMember(Name = "isHidden")]
         public virtual bool IsHidden => false;
 
         /// <inheritdoc />
         /// <remarks>Defaults to true.</remarks>
-        [DataMember(Name = "isStored")]
         public virtual bool IsStored => true;
 
         /// <inheritdoc />
         /// <remarks>Defaults to true.</remarks>
-        [DataMember(Name = "supportsValidation")]
         public virtual bool SupportsValidation => true;
 
         /// <inheritdoc />
         /// <remarks>Defaults to true.</remarks>
-        [DataMember(Name = "supportsLabel")]
         public virtual bool SupportsLabel => true;
 
         /// <inheritdoc />
-        [DataMember(Name = "category")]
         public virtual string Category => FormFieldConstants.Categories.Uncategorized;
 
         /// <inheritdoc />
--- src/Formulate.Core/FormHandlers/FormHandlerDefinition.cs
+++ src/Formulate.Core/FormHandlers/FormHandlerDefinition.cs
@@ -2,32 +2,25 @@
 {
     // Namespaces.
     using System;
-    using System.Runtime.Serialization;
 
     /// <summary>
     /// An abstract class for creating a form handler definition.
     /// </summary>
-    [DataContract]
     public abstract class FormHandlerDefinition : IFormHandlerDefinition
     {
         /// <inheritdoc />
-        [DataMember(Name = "icon")]
         public abstract string Icon { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "kindId")]
         public abstract Guid KindId { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "definitionLabel")]
         public abstract string DefinitionLabel { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "directive")]
         public abstract string Directive { get; }
 
         /// <inheritdoc />
-        [DataMember(Name = "category")]
         public virtual string Category => FormHandlerConstants.Categories.Uncategorized;
 
         /// <inheritdoc />
