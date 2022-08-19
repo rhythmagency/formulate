05d14cf1614066e1f0157cd85f7a4b1aeb1963af
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-19 13:16:00 -0400

    Updated TreeTypes xml-docs

--- src/Formulate.BackOffice/TreeTypes.cs
+++ src/Formulate.BackOffice/TreeTypes.cs
@@ -1,32 +1,32 @@
 ﻿namespace Formulate.BackOffice
 {
     /// <summary>
-    /// The tree root types used by Formulate in the back office.
+    /// The core tree types used by Formulate in the back office.
     /// </summary>
     public enum TreeTypes
     {
         /// <summary>
-        /// An unknown tree root type.
+        /// An unknown tree type.
         /// </summary>
         Unknown,
 
         /// <summary>
-        /// The form tree root type.
+        /// The form tree type.
         /// </summary>
         Forms,
 
         /// <summary>
-        /// The data values tree root type.
+        /// The data values tree type.
         /// </summary>
         DataValues,
 
         /// <summary>
-        /// The layouts tree root type.
+        /// The layouts tree type.
         /// </summary>
         Layouts,
 
         /// <summary>
-        /// The validations tree root type.
+        /// The validations tree type.
         /// </summary>
         Validations
     }
