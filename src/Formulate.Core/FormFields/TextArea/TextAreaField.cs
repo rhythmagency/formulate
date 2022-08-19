b54c69a5f171d1b8bd0461c68331a8525e66727a
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-19 17:58:12 -0400

    Added initial text area field

---
+++ src/Formulate.Core/FormFields/TextArea/TextAreaField.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Formulate.Core.Validations;
+
+namespace Formulate.Core.FormFields.TextArea
+{
+    /// <summary>
+    /// A text area field.
+    /// </summary>
+    public sealed class TextAreaField : FormField
+    {
+        /// <inheritdoc />
+        public TextAreaField(IFormFieldSettings settings) : base(settings)
+        {
+        }
+
+        /// <inheritdoc />
+        public TextAreaField(IFormFieldSettings settings, IReadOnlyCollection<IValidation> validations) : base(settings, validations)
+        {
+        }
+    }
+}
---
+++ src/Formulate.Core/FormFields/TextArea/TextAreaFieldDefinition.cs
@@ -0,0 +1,58 @@
+﻿namespace Formulate.Core.FormFields.TextArea
+{
+    // Namespaces.
+    using System;
+
+    /// <summary>
+    /// A text area form field definition.
+    /// </summary>
+    public sealed class TextAreaFieldDefinition : FormFieldDefinition<TextAreaField>
+    {
+        /// <summary>
+        /// Constants related to <see cref="TextFieldDefinition"/>.
+        /// </summary>
+        public static class Constants
+        {
+            /// <summary>
+            /// The kind ID.
+            /// </summary>
+            public const string KindId = "9DA843594D0B494491449F8CCAE7A4DA";
+
+            /// <summary>
+            /// The name.
+            /// </summary>
+            public const string Name = "Text Area";
+
+            /// <summary>
+            /// The icon.
+            /// </summary>
+            public const string Icon = "icon-formulate-textarea";
+
+            /// <summary>
+            /// The Angular JS directive.
+            /// </summary>
+            public const string Directive = "formulate-text-area-field";
+        }
+
+        /// <inheritdoc />
+        public override string Directive => Constants.Directive;
+
+        /// <inheritdoc />
+        public override string Name => Constants.Name;
+
+        /// <inheritdoc />
+        public override string Category => FormFieldConstants.Categories.Inputs;
+
+        /// <inheritdoc />
+        public override string Icon => Constants.Icon;
+
+        /// <inheritdoc />
+        public override Guid KindId => Guid.Parse(Constants.KindId);
+
+        /// <inheritdoc />
+        public override FormField CreateField(IFormFieldSettings settings)
+        {
+            return new TextAreaField(settings);
+        }
+    }
+}
