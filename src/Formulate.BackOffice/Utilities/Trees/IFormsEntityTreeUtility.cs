d69805fed12662910c763f2d28108882b2326233
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-19 09:40:31 -0400

    Added separate contracts for Entity Tree Utilities

---
+++ src/Formulate.BackOffice/Utilities/Trees/IFormsEntityTreeUtility.cs
@@ -0,0 +1,11 @@
+﻿namespace Formulate.BackOffice.Utilities.Trees
+{
+    using Formulate.BackOffice.Trees;
+
+    /// <summary>
+    /// A contract for creating a <see cref="IEntityTreeUtility"/> which works directly with the <see cref="FormulateFormsTreeController"/>.
+    /// </summary>
+    public interface IFormsEntityTreeUtility : IEntityTreeUtility
+    {
+    }
+}
---
+++ src/Formulate.BackOffice/Utilities/Trees/ILayoutsEntityTreeUtility.cs
@@ -0,0 +1,11 @@
+﻿namespace Formulate.BackOffice.Utilities.Trees
+{
+    using Formulate.BackOffice.Trees;
+
+    /// <summary>
+    /// A contract for creating a <see cref="IEntityTreeUtility"/> which works directly with the <see cref="FormulateLayoutsTreeController"/>.
+    /// </summary>
+    public interface ILayoutsEntityTreeUtility : IEntityTreeUtility
+    {
+    }
+}
