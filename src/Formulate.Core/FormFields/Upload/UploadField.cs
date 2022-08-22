bd2d55324bc214809e764094d2fde2f896ca8fef
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-22 17:22:49 -0400

    Added upload field

---
+++ src/Formulate.Core/FormFields/Upload/UploadField.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Formulate.Core.Validations;
+
+namespace Formulate.Core.FormFields.Upload
+{
+    /// <summary>
+    /// An upload field.
+    /// </summary>
+    public sealed class UploadField : FormField
+    {
+        /// <inheritdoc />
+        public UploadField(IFormFieldSettings settings) : base(settings)
+        {
+        }
+
+        /// <inheritdoc />
+        public UploadField(IFormFieldSettings settings, IReadOnlyCollection<IValidation> validations) : base(settings, validations)
+        {
+        }
+    }
+}
---
+++ src/Formulate.Core/FormFields/Upload/UploadFieldDefinition.cs
@@ -0,0 +1,58 @@
+﻿namespace Formulate.Core.FormFields.Upload
+{
+    // Namespaces.
+    using System;
+
+    /// <summary>
+    /// An upload field definition.
+    /// </summary>
+    public sealed class UploadFieldDefinition : FormFieldDefinition<UploadField>
+    {
+        /// <summary>
+        /// Constants related to <see cref="UploadFieldDefinition"/>.
+        /// </summary>
+        public static class Constants
+        {
+            /// <summary>
+            /// The kind ID.
+            /// </summary>
+            public const string KindId = "DFEFA5EC02004806A2AB0AB22058021D";
+
+            /// <summary>
+            /// The name.
+            /// </summary>
+            public const string Name = "Upload";
+
+            /// <summary>
+            /// The icon.
+            /// </summary>
+            public const string Icon = "icon-formulate-upload";
+
+            /// <summary>
+            /// The Angular JS directive.
+            /// </summary>
+            public const string Directive = "formulate-upload-field";
+        }
+
+        /// <inheritdoc />
+        public override string Directive => Constants.Directive;
+
+        /// <inheritdoc />
+        public override string Name => Constants.Name;
+
+        /// <inheritdoc />
+        public override string Category => FormFieldConstants.Categories.Inputs;
+
+        /// <inheritdoc />
+        public override string Icon => Constants.Icon;
+
+        /// <inheritdoc />
+        public override Guid KindId => Guid.Parse(Constants.KindId);
+
+        /// <inheritdoc />
+        public override FormField CreateField(IFormFieldSettings settings)
+        {
+            return new UploadField(settings);
+        }
+    }
+}
