4d0f24560150e8d09617a690a6ecf04b8e48b6d7
jamiepollock <jamie.does.umbraco@gmail.com>
2022-08-23 12:35:55 -0400

    Added data values picker to drop down config directive

--- src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/editors/formFields/drop-down/drop-down-field.editor.js
+++ src/Formulate.BackOffice.StaticAssets/App_Plugins/FormulateBackOffice/directives/editors/formFields/drop-down/drop-down-field.editor.js
@@ -15,96 +15,9 @@
     }
 
     // Controller.
-    function controller($scope, editorService, formulateDataValues) {
+    function controller($scope) {
         $scope.configuration = $scope.configuration || {};
-
-        // Variables.
-        var services = {
-            $scope: $scope,
-            editorService: editorService,
-            formulateDataValues: formulateDataValues
-        };
-
-        // Set scope variables.
-        $scope.pickDataValue = performPickDataValue(services);
-        $scope.removeDataValue = performRemoveDataValue(services);
-
-        // Refresh the data value info.
-        refreshDataValue(services);
-
     }
-
-    // Allows the user to pick their data value.
-    function performPickDataValue(services) {
-        var $scope = services.$scope;
-        var editorService = services.editorService;
-        return function () {
-            const config = {
-                section: 'formulate',
-                treeAlias: 'dataValues',
-                multiPicker: false,
-                entityType: 'dataValues',
-                filter: (node) => {
-                    return node.nodeType === 'Folder';
-                },
-                filterCssClass: 'not-allowed',
-                select: (node) => {
-                    if (node) {
-                        $scope.configuration.dataValue = node.id;
-                    }
-
-                    refreshDataValue(services);
-
-                    editorService.close();
-                },
-                submit: () => {
-                    editorService.close();
-                },
-                close: () => {
-                    editorService.close();
-                }
-            };
-
-            editorService.treePicker(config);
-
-        };
-    }
-
-    function performRemoveDataValue(services) {
-        var $scope = services.$scope;
-
-        return function () {
-            $scope.configuration.dataValue = undefined;
-            $scope.dataValue = {};
-        }
-    }
-
-    // Update the scope with info about the data value based on its ID.
-    function refreshDataValue(services) {
-
-        // Variables.
-        var $scope = services.$scope;
-        var formulateDataValues = services.formulateDataValues;
-
-        // Return early if there is no data value to get info about.
-        if (!$scope.configuration.dataValue) {
-            return;
-        }
-
-        // Get info about data value.
-        formulateDataValues.getDataValueInfo($scope.configuration.dataValue)
-            .then(function (data) {
-
-                if (data) {
-                    $scope.dataValue = {
-                        id: data.id,
-                        name: data.name
-                    };
-                }
-            });
-
-    }
-
     // Associate directive/controller.
     angular.module("umbraco").directive("formulateDropDownField", directive);
 
