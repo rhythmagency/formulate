0057bd0c0e9e8951b7841af2761bb42611911832
jamie.pollock <jamie.does.umbraco@gmail.com>
2022-09-08 15:34:03 -0400

    Added IHaveDirective
    This is the start of separating out the Directive from the IDefinition as it's not needed by all IDefinition implementations.

---
+++ src/formulate.core/Types/IHaveDirective.cs
@@ -0,0 +1,13 @@
+﻿namespace Formulate.Core.Types
+{
+    /// <summary>
+    /// For implementations that have a directive.
+    /// </summary>
+    public interface IHaveDirective
+    {
+        /// <summary>
+        /// Gets the directive.
+        /// </summary>
+        string Directive { get; }
+    }
+}
