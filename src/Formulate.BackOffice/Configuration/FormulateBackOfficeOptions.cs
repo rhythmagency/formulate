e0a1aa402956ace23dfae63fcff72d95f652f799
jamie.pollock <jamie.does.umbraco@gmail.com>
2022-09-08 19:06:59 -0400

    Added optional Default Template ID

--- src/Formulate.BackOffice/Configuration/FormulateBackOfficeOptions.cs
+++ src/Formulate.BackOffice/Configuration/FormulateBackOfficeOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace Formulate.BackOffice.Configuration
+﻿using System;
+using Formulate.Core.Templates;
+
+namespace Formulate.BackOffice.Configuration
 {
     public sealed class FormulateBackOfficeOptions
     {
@@ -9,5 +12,12 @@
         /// </summary>
         /// <remarks>This defaults to false.</remarks>
         public bool UseDefaultFolderIcon { get; set; } = false;
+
+
+        /// <summary>
+        /// Gets the default template ID to be used when creating new layouts.
+        /// </summary>
+        /// <remarks>If no value is provided the first registered <see cref="ITemplateDefinition"/>'s <see cref="KindId"/> will be used instead.</remarks>
+        public Guid? DefaultTemplateId { get; set; }
     }
 }
