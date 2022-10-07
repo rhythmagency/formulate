93afd537245b139852b1bf084ad7a9129d77bea2
jamiepollock <jamie.does.umbraco@gmail.com>
2022-10-07 11:42:08 -0400

    Added FormSubmittingNotification
    This allows developers to modify the ExtraContext property of a FormSubmissionRequest. All other properties should be init or read only.

---
+++ src/Formulate.Core/Notifications/FormSubmittingNotification.cs
@@ -0,0 +1,22 @@
+﻿namespace Formulate.Core.Notifications
+{
+    using Formulate.Core.Submissions.Requests;
+    using Umbraco.Cms.Core.Notifications;
+
+    /// <summary>
+    /// A notification for modifying a <see cref="FormSubmissionRequest"/> before it is handled by form handlers.
+    /// </summary>
+    /// <remarks>This is primarily for modifying the ExtraContext property. Other propertoes in the request should be read only.</remarks>
+    public sealed class FormSubmittingNotification : INotification
+    {
+        public FormSubmittingNotification(FormSubmissionRequest request)
+        {
+            Request = request;
+        }
+
+        /// <summary>
+        /// Gets the request.
+        /// </summary>
+        public FormSubmissionRequest Request { get; init; }
+    }
+}
--- src/Formulate.Core/Utilities/Submissions/SubmitFormSubmissionRequest.cs
+++ src/Formulate.Core/Utilities/Submissions/SubmitFormSubmissionRequest.cs
@@ -1,23 +1,41 @@
 ﻿namespace Formulate.Core.Utilities.Submissions
 {
     using Formulate.Core.FormHandlers;
+    using Formulate.Core.Notifications;
     using Formulate.Core.Submissions.Requests;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using Umbraco.Cms.Core.Scoping;
 
     internal sealed class SubmitFormSubmissionRequest : ISubmitFormSubmissionRequest
     {
+        /// <summary>
+        /// The core scope provider.
+        /// </summary>
+        private readonly ICoreScopeProvider _coreScopeProvider;
+
+        /// <summary>
+        /// The form handler factory.
+        /// </summary>
         private readonly IFormHandlerFactory _formHandlerFactory;
-        
-        public SubmitFormSubmissionRequest(IFormHandlerFactory formHandlerFactory)
+
+        public SubmitFormSubmissionRequest(ICoreScopeProvider coreScopeProvider, IFormHandlerFactory formHandlerFactory)
         {
+            _coreScopeProvider = coreScopeProvider;
             _formHandlerFactory = formHandlerFactory;
         }
 
         public async Task<bool> SubmitAsync(FormSubmissionRequest input, CancellationToken cancellationToken)
+        {
+            var modifiedInput = NotifySubmission(input);
+
+            return await PerformSubmitAsync(modifiedInput, cancellationToken);
+        }
+
+        private async Task<bool> PerformSubmitAsync(FormSubmissionRequest input, CancellationToken cancellationToken)
         {
             var form = input.Form;
             var formHandlers = CreateFormHandlers(form.Handlers);
@@ -28,7 +46,7 @@
                 {
                     formHandler.Handle(input);
                 }
-                
+
                 if (item is AsyncFormHandler asyncFormHandler)
                 {
                     await asyncFormHandler.HandleAsync(input, cancellationToken);
@@ -36,6 +54,23 @@
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Notifies custom notification handlers that form is being submitted.
+        /// </summary>
+        /// <param name="input">The request.</param>
+        /// <returns>A potentially modified <see cref="FormSubmissionRequest"/>.</returns>
+        private FormSubmissionRequest NotifySubmission(FormSubmissionRequest input)
+        {
+            using (var scope = _coreScopeProvider.CreateCoreScope())
+            {
+                var notification = new FormSubmittingNotification(input);
+                scope.Notifications.Publish(notification);
+                scope.Complete();
+
+                return notification.Request;
+            }
         }
 
         private IReadOnlyCollection<IFormHandler> CreateFormHandlers(PersistedFormHandler[] items)
