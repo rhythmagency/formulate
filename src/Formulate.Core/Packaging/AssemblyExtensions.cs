674ded76119b84d7872fe8d209c0433990e2a82a
jamiepollock <jamie.does.umbraco@gmail.com>
2022-10-14 10:04:53 -0400

    Added extensions to be used during packaging

---
+++ src/Formulate.Core/Packaging/AssemblyExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Formulate.Core.Packaging
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Extension methods that augement the <see cref="Assembly"/> class.
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="Version"/> from the current <see cref="Assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The current assembly</param>
+        /// <returns>A <see cref="Version"/>.</returns>
+        public static Version GetVersionOrDefault(this Assembly assembly)
+        {
+            return assembly.GetVersionOrDefault(new Version());
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Version"/> from the current <see cref="Assembly"/>.
+        /// </summary>
+        /// <param name="assembly">The current assembly</param>
+        /// <param name="defaultValue">The default value if no version is found.</param>
+        /// <returns>A <see cref="Version"/>.</returns>
+        public static Version GetVersionOrDefault(this Assembly assembly, Version defaultValue)
+        {
+            var name = assembly.GetName();
+            var version = name.Version ?? defaultValue;
+
+            return version;
+        }
+    }
+}
---
+++ src/Formulate.Core/Packaging/TypeExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Formulate.Core.Packaging
+{
+    using System;
+
+    /// <summary>
+    /// Extension methods that augement the <see cref="Type"/> class.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Gets the version from the current <see cref="Type"/> class's Assembly property.
+        /// </summary>
+        /// <param name="type">The current type.</param>
+        /// <returns>A <see cref="Version"/>.</returns>
+        public static Version GetAssemblyVersionOrDefault(this Type type)
+        {
+            return type.GetAssemblyVersionOrDefault(new Version());
+        }
+
+        /// <summary>
+        /// Gets the version from the current <see cref="Type"/> class's Assembly property.
+        /// </summary>
+        /// <param name="type">The current type.</param>
+        /// <param name="defaultValue">The default value if no version is found.</param>
+        /// <returns>A <see cref="Version"/>.</returns>
+        public static Version GetAssemblyVersionOrDefault(this Type type, Version defaultValue)
+        {
+            return type.Assembly.GetVersionOrDefault(defaultValue);
+        }
+    }
+}
